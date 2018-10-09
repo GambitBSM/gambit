66d6ab048682ac95e65cc64a0d060f798398b914
Tomas Gonzalo <tomas.gonzalo@monash.edu>
2018-10-09 13:42:10 +1100

    Added NUGM model

---
+++ Models/include/gambit/Models/models/NUGM.hpp
@@ -0,0 +1,32 @@
+//   GAMBIT: Global and Modular BSM Inference Tool
+//   *********************************************
+///  \file
+///
+///  NUGM model declaration. 
+///
+///  *********************************************
+///
+///  Authors (add name and date if you modify):
+///   
+///  \author Tomas Gonzalo  
+///          (tomas.gonzalo@monash.edu)
+///  \date 2018 Oct
+///
+///  *********************************************
+
+#ifndef __NUGM_hpp__
+#define __NUGM_hpp__
+
+// Must include models which are targets of translation functions
+#include "gambit/Models/models/MSSM30atMGUT.hpp" 
+
+#define MODEL NUGM
+#define PARENT MSSM63atMGUT
+  START_MODEL
+  DEFINEPARS(M0,M1,M2,M3,A0,TanBeta,SignMu)
+  INTERPRET_AS_PARENT_FUNCTION(NUGM_to_MSSM63atMGUT)
+#undef PARENT
+#undef MODEL
+
+
+#endif
