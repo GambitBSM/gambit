90c40c286edb5e4a99d9a8917d088e6ee6b20e90
Tomas Gonzalo <tomas.gonzalo@monash.edu>
2019-01-11 15:17:26 +1100

    Fix loop function issue in loop decays

--- Backends/patches/sarah-spheno/4.0.3/NMSSM/make_sarah-spheno_NMSSM_patch.sh
+++ Backends/patches/sarah-spheno/4.0.3/NMSSM/make_sarah-spheno_NMSSM_patch.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
-cd ../../../downloaded/
+cd ../../../../downloaded/
 tar -xvf SPheno-4.0.3.tar.gz
-echo "diff -rupN SPheno-4.0.3/Makefile ../installed/sphenonmssm/4.0.3/Makefile" > patch_sphenonmssm_4.0.3.dif
-diff -rupN SPheno-4.0.3/Makefile ../installed/sphenonmssm/4.0.3/Makefile >> patch_sphenonmssm_4.0.3.dif
-echo "diff -rupN SPheno-4.0.3/NMSSM/Makefile ../installed/sphenonmssm/4.0.3/NMSSM/Makefile" >> patch_sphenonmssm_4.0.3.dif
-diff -rupN  SPheno-4.0.3/NMSSM/Makefile ../installed/sphenonmssm/4.0.3/NMSSM/Makefile >> patch_sphenonmssm_4.0.3.dif
-echo "diff -rupN SPheno-4.0.3/src/Makefile ../installed/sphenonmssm/4.0.3/src/Makefile" >> patch_sphenonmssm_4.0.3.dif
-diff -rupN SPheno-4.0.3/src/Makefile ../installed/sphenonmssm/4.0.3/src/Makefile >> patch_sphenonmssm_4.0.3.dif
-echo "diff -rupN SPheno-4.0.3/NMSSM/SPhenoNMSSM.f90 ../installed/sphenonmssm/4.0.3/NMSSM/SPhenoNMSSM.f90" >> patch_sphenonmssm_4.0.3.dif
-diff -rupN SPheno-4.0.3/NMSSM/SPhenoNMSSM.f90 ../installed/sphenonmssm/4.0.3/NMSSM/SPhenoNMSSM.f90  >> patch_sphenonmssm_4.0.3.dif
-echo "diff -rupN SPheno-4.0.3/src/Control.F90 ../installed/sphenonmssm/4.0.3/src/Control.F90" >> patch_sphenonmssm_4.0.3.dif
-diff -rupN SPheno-4.0.3/src/Control.F90 ../installed/sphenonmssm/4.0.3/src/Control.F90 >> patch_sphenonmssm_4.0.3.dif
-echo "diff -rupN SPheno-4.0.3/NMSSM/BranchingRatios_NMSSM.f90 ../installed/sphenonmssm/4.0.3/NMSSM/BranchingRatios_NMSSM.f90" >> patch_sphenonmssm_4.0.3.dif
-diff -rupN SPheno-4.0.3/NMSSM/BranchingRatios_NMSSM.f90 ../installed/sphenonmssm/4.0.3/NMSSM/BranchingRatios_NMSSM.f90 >> patch_sphenonmssm_4.0.3.dif
-mv patch_sphenonmssm_4.0.3.dif ../patches/sphenonmssm/4.0.3
-cd ../patches/sphenonmssm/4.0.3
+mkdir SPheno-4.0.3/NMSSM
+cp -r ../../Models/data/SARAH/NMSSM/EWSB/SPheno/* ./SPheno-4.0.3/NMSSM
+echo "diff -rupN SPheno-4.0.3/Makefile ../installed/sarah-spheno/4.0.3/NMSSM/Makefile" > patch_sarah-spheno_4.0.3_NMSSM.dif
+diff -rupN SPheno-4.0.3/Makefile ../installed/sarah-spheno/4.0.3/NMSSM/Makefile >> patch_sarah-spheno_4.0.3_NMSSM.dif
+echo "diff -rupN SPheno-4.0.3/NMSSM/Makefile ../installed/sarah-spheno/4.0.3/NMSSM/NMSSM/Makefile" >> patch_sarah-spheno_4.0.3_NMSSM.dif
+diff -rupN  SPheno-4.0.3/NMSSM/Makefile ../installed/sarah-spheno/4.0.3/NMSSM/NMSSM/Makefile >> patch_sarah-spheno_4.0.3_NMSSM.dif
+echo "diff -rupN SPheno-4.0.3/src/Makefile ../installed/sarah-spheno/4.0.3/NMSSM/src/Makefile" >> patch_sarah-spheno_4.0.3_NMSSM.dif
+diff -rupN SPheno-4.0.3/src/Makefile ../installed/sarah-spheno/4.0.3/NMSSM/src/Makefile >> patch_sarah-spheno_4.0.3_NMSSM.dif
+echo "diff -rupN SPheno-4.0.3/NMSSM/SPhenoNMSSM.f90 ../installed/sarah-spheno/4.0.3/NMSSM/NMSSM/SPhenoNMSSM.f90" >> patch_sarah-spheno_4.0.3_NMSSM.dif
+diff -rupN SPheno-4.0.3/NMSSM/SPhenoNMSSM.f90 ../installed/sarah-spheno/4.0.3/NMSSM/NMSSM/SPhenoNMSSM.f90  >> patch_sarah-spheno_4.0.3_NMSSM.dif
+echo "diff -rupN SPheno-4.0.3/src/Control.F90 ../installed/sarah-spheno/4.0.3/NMSSM/src/Control.F90" >> patch_sarah-spheno_4.0.3_NMSSM.dif
+diff -rupN SPheno-4.0.3/src/Control.F90 ../installed/sarah-spheno/4.0.3/NMSSM/src/Control.F90 >> patch_sarah-spheno_4.0.3_NMSSM.dif
+echo "diff -rupN SPheno-4.0.3/NMSSM/BranchingRatios_NMSSM.f90 ../installed/sarah-spheno/4.0.3/NMSSM/NMSSM/BranchingRatios_NMSSM.f90" >> patch_sarah-spheno_4.0.3_NMSSM.dif
+diff -rupN SPheno-4.0.3/NMSSM/BranchingRatios_NMSSM.f90 ../installed/sarah-spheno/4.0.3/NMSSM/NMSSM/BranchingRatios_NMSSM.f90 >> patch_sarah-spheno_4.0.3_NMSSM.dif
+echo "diff -rupN SPheno-4.0.3/NMSSM/AddLoopFunctions.f90 ../installed/sarah-spheno/4.0.3/NMSSM/NMSSM/AddLoopFunctions.f90" >> patch_sarah-spheno_4.0.3_NMSSM.dif
+diff -rupN SPheno-4.0.3/NMSSM/AddLoopFunctions.f90 ../installed/sarah-spheno/4.0.3/NMSSM/NMSSM/AddLoopFunctions.f90 >> patch_sarah-spheno_4.0.3_NMSSM.dif
+mv patch_sarah-spheno_4.0.3_NMSSM.dif ../patches/sarah-spheno/4.0.3/NMSSM
+rm -r SPheno-4.0.3
+cd ../patches/sarah-spheno/4.0.3/NMSSM
