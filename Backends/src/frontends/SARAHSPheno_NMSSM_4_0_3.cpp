a0f0ea2321e94c313e235c9f5426ece05d128d6b
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2019-01-12 17:40:07 +0100

    Fixed typo in SPheno NMSSM SLHA comments 	modified:   SARAHSPheno_NMSSM_4_0_3.cpp

--- Backends/src/frontends/SARAHSPheno_NMSSM_4_0_3.cpp
+++ Backends/src/frontends/SARAHSPheno_NMSSM_4_0_3.cpp
@@ -947,8 +947,8 @@ BE_NAMESPACE
       for(int i=1; i<=3; i++)
         for(int j=1; j<=3; j++)
         {
-          slha["VCKM"][""] << i << j << CKM_Q(i,j).re << "V_" << i << j;
-          slha["IMVCKM"][""] << i << j << CKM_Q(i,j).im << "Im(V_" << i << j << ")";
+          slha["VCKM"][""] << i << j << CKM_Q(i,j).re << "# V_" << i << j;
+          slha["IMVCKM"][""] << i << j << CKM_Q(i,j).im << "# Im(V_" << i << j << ")";
         }
   
     }
