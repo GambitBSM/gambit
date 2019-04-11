2b397c9fc26f78c72ba7292c3642efd18ed18541
Filip Rajec <filiprajec@yahoo.com>
2019-04-11 18:26:35 +0930

    (debug commit)

--- Models/src/models/THDMI_physical.cpp
+++ Models/src/models/THDMI_physical.cpp
@@ -50,17 +50,7 @@ void MODEL_NAMESPACE::THDMI_physical_to_THDMI(const ModelParameters &myP, ModelP
   USE_MODEL_PIPE(FRIEND) // get pipe for "interpret as FRIEND" function
   logger()<<"Running interpret_as_FRIEND calculations for THDMI_physical --> THDMI"<<LogTags::info<<EOM;
 
-  // SMInputs& sminputs();
-  // fill only what is necessary for conversion
-  // sminputs.GF =  Dep::SMINPUTS->GF;
-  // Gambit::dep_bucket<Gambit::SMInputs> sminputs =;
-  // sminputs = sminputs->;
-
   const SMInputs& sminputs = *Dep::SMINPUTS;
-
-  // double GF = Dep::SMINPUTS->GF;
-  // double v2 = 1.0/(sqrt(2)*GF);
-
   std::map<std::string, double> basis = SpecBit::create_empty_THDM_basis();
 
   basis["m_h"] = myP.getValue("m_h");
@@ -73,15 +63,7 @@ void MODEL_NAMESPACE::THDMI_physical_to_THDMI(const ModelParameters &myP, ModelP
   basis["lambda6"] = myP.getValue("lambda_6");
   basis["lambda7"] = myP.getValue("lambda_7");
 
-  // // Check for problematic parameter choices
-  // if (m_h>m_H) {
-  //   cerr << "WARNING: Cannot set physical masses such that m_H < m_h\n"; 
-  //   // throw error
-  // }
-  // if ((tanb<=0)||(abs(sba)>1)||(m_h<0)||(m_H<0)||(m_A<0)||(m_Hp<0)) {
-  //   cerr << "WARNING: Problematic input parameter choice.\n"; 
-  //   // throw error
-  // }
+  // Check for problematic parameter choices here
 
   SpecBit::fill_generic_THDM_basis(basis, sminputs);
 
@@ -94,7 +76,6 @@ void MODEL_NAMESPACE::THDMI_physical_to_THDMI(const ModelParameters &myP, ModelP
   targetP.setValue("lambda_7", basis["lambda7"] );
   targetP.setValue("m12_2", basis["m12_2"] );
   targetP.setValue("tanb", basis["tanb"] );
-
 
   // Done! Check that everything is ok if desired.
   #ifdef THDM_DBUG
@@ -109,79 +90,31 @@ void MODEL_NAMESPACE::THDMI_physical_to_THDM (const ModelParameters &myP, ModelP
   USE_MODEL_PIPE(PARENT) // get pipe for "interpret as PARENT" function
   logger()<<"Running interpret_as_parent calculations for THDMI --> THDMIatQ.."<<LogTags::info<<EOM;
 
-  targetP.setValue("Qin",80.39);
-  targetP.setValue("QrunTo", 173.15);
+  const SMInputs& sminputs = *Dep::SMINPUTS;
+  std::map<std::string, double> basis = SpecBit::create_empty_THDM_basis();
 
-  double lambda1, lambda2, lambda3, lambda4, lambda5, lambda6, lambda7;
-  double tanb, m12_2;
+  basis["m_h"] = myP.getValue("m_h");
+  basis["m_H"] = myP.getValue("m_H");
+  basis["m_A"]= myP.getValue("m_A");
+  basis["m_Hp"]= myP.getValue("m_Hp");
+  basis["m12_2"] = myP.getValue("m12_2");
+  basis["tanb"] = myP.getValue("tanb");
+  basis["sba"] = myP.getValue("sba");
+  basis["lambda6"] = myP.getValue("lambda_6");
+  basis["lambda7"] = myP.getValue("lambda_7");
 
-  // to be implemented as a capability in SpecBit 
-  // **
+  SpecBit::fill_generic_THDM_basis(basis, sminputs);
+  SpecBit::fill_higgs_THDM_basis(basis, sminputs);
 
-  // const SMInputs& SM = *Dep::SMINPUTS;
-  double GF = Dep::SMINPUTS->GF;
-  double v2 = 1./(sqrt(2)*GF);
-  // double v2 = 247.0*247.0;
-
-  double sba = myP.getValue("sba");
-  tanb = myP.getValue("tanb");
-
-  double b = atan(tanb);
-  double beta = - b; //for all equations needed when converting to coupling basis
-  double sb=sin(beta);
-  double s2b=sin(2.*beta);
-  double s3b=sin(3.*beta);
-  double s2b2=s2b*s2b;
-  double sb2=sb*sb;
-  double sb4=sb2*sb2;
-  double cb=cos(beta);
-  double c2b=cos(2.*beta);
-  double c3b=cos(3.*beta);
-  double cb2=cb*cb;
-  double cb4=cb2*cb2;
-
-  double cba = sqrt(1-sba*sba); //should always be the positive value
-  double s2ba = 2*sba*cba;
-  double c2ba = sqrt(1-s2ba*s2ba);
-  double t2ba = s2ba/c2ba;
-
-  double Lambda1 = myP.getValue("Lambda1");
-  double Lambda2 = myP.getValue("Lambda2");
-  double Lambda3 = myP.getValue("Lambda3");
-  double Lambda4 = myP.getValue("Lambda4");
-  double Lambda5 = myP.getValue("Lambda5");
-  double Lambda7 = myP.getValue("Lambda7");
-  double M22_2 = myP.getValue("M22_2");
-
-  double m_Hp2 = M22_2 + 0.5*v2*Lambda3;
-  double mA2 = m_Hp2 - 0.5*v2*(Lambda5-Lambda4);
-
-  double Lambda6 = t2ba/(2*v2) * (mA2 + v2*(Lambda5-Lambda1));
-  if (sba<0.0) Lambda6 = -Lambda6;
-
-  m12_2 = -0.5*Lambda6*v2;
-
-  double lambda345=Lambda3+Lambda4+Lambda5;
-  // See hep-ph/0504050
-  lambda1 =  Lambda1*cb4+Lambda2*sb4+0.5*lambda345*s2b2+2.*s2b*(cb2*Lambda6+sb2*Lambda7);
-  lambda2 =  Lambda1*sb4+Lambda2*cb4+0.5*lambda345*s2b2-2.*s2b*(sb2*Lambda6+cb2*Lambda7);
-  lambda3 =  0.25*s2b2*(Lambda1+Lambda2-2*lambda345)+Lambda3-s2b*c2b*(Lambda6-Lambda7);
-  lambda4 =  0.25*s2b2*(Lambda1+Lambda2-2*lambda345)+Lambda4-s2b*c2b*(Lambda6-Lambda7);
-  lambda5 =  0.25*s2b2*(Lambda1+Lambda2-2*lambda345)+Lambda5-s2b*c2b*(Lambda6-Lambda7);
-  lambda6 = -0.5*s2b*(Lambda1*cb2-Lambda2*sb2-lambda345*c2b)+cb*c3b*Lambda6+sb*s3b*Lambda7;
-  lambda7 = -0.5*s2b*(Lambda1*sb2-Lambda2*cb2+lambda345*c2b)+sb*s3b*Lambda6+cb*c3b*Lambda7;
-
-  // **
-
-  targetP.setValue("lambda_1", lambda1 );
-  targetP.setValue("lambda_2", lambda2 );
-  targetP.setValue("lambda_3", lambda3 );
-  targetP.setValue("lambda_4", lambda4 );
-  targetP.setValue("lambda_5", lambda5 );
-  targetP.setValue("lambda_6", lambda6 );
-  targetP.setValue("lambda_7", lambda7 );
-  targetP.setValue("m12_2", m12_2 );
-  targetP.setValue("tanb", tanb );
+  targetP.setValue("Lambda_1", basis["Lambda1"] );
+  targetP.setValue("Lambda_2", basis["Lambda2"] );
+  targetP.setValue("Lambda_3", basis["Lambda3"] );
+  targetP.setValue("Lambda_4", basis["Lambda4"] );
+  targetP.setValue("Lambda_5", basis["Lambda5"] );
+  targetP.setValue("Lambda_6", basis["Lambda6"] );
+  targetP.setValue("Lambda_7", basis["Lambda7"] );
+  targetP.setValue("m22_2", basis["M22_2"] );
+  targetP.setValue("tanb", basis["tanb"] );
 
   std::vector<std::string> yukawa_keys = {"yu2_re_11", "yu2_im_11", "yu2_re_12", "yu2_im_12", "yu2_re_13", "yu2_im_13",
                                           "yu2_re_21", "yu2_im_21", "yu2_re_22", "yu2_im_22", "yu2_re_23", "yu2_im_23",
@@ -197,6 +130,8 @@ for (auto &yukawa_key : yukawa_keys) // access by reference to avoid copying
 {  
     targetP.setValue(yukawa_key, 0.0);
 }
+  targetP.setValue("Qin",80.39);
+  targetP.setValue("QrunTo", 173.15);
 
   // Done! Check that everything is ok if desired.
   #ifdef THDM_DBUG
