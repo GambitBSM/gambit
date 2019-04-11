666323d864b2ece74f6699beebafbaa6e1f631f2
Filip Rajec <filiprajec@yahoo.com>
2019-04-12 00:24:19 +0930

    (debug commit)

--- SpecBit/include/gambit/SpecBit/THDMSpec.hpp
+++ SpecBit/include/gambit/SpecBit/THDMSpec.hpp
@@ -404,7 +404,7 @@ namespace Gambit
          const double EMPTY = -1E10;
          std::map<std::string, double> basis;
          // create a complete list of keys for basis
-         const std::vector<std::string> basis_keys{"lambda1", "lambda2", "lambda3", "lambda4", "lambda5", "lambda6", "lambda7,", "m12_2", "m11_2", "m22_2", \
+         const std::vector<std::string> basis_keys{"lambda1", "lambda2", "lambda3", "lambda4", "lambda5", "lambda6", "lambda7", "m12_2", "m11_2", "m22_2", \
                                              "Lambda1", "Lambda2", "Lambda3", "Lambda4", "Lambda5", "Lambda6", "Lambda7", "M12_2", "M11_2", "M22_2", \
                                              "m_h", "m_H", "m_A", "m_Hp", "tanb", "sba","alpha"};
          // fill entries in basis
@@ -416,7 +416,7 @@ namespace Gambit
 
       inline void print_THDM_spectrum(std::map<std::string, double>& basis){
          const double EMPTY = -1E10;
-         const std::vector<std::string> basis_keys{"lambda1", "lambda2", "lambda3", "lambda4", "lambda5", "lambda6", "lambda7,", "m12_2", "m11_2", "m22_2", \
+         const std::vector<std::string> basis_keys{"lambda1", "lambda2", "lambda3", "lambda4", "lambda5", "lambda6", "lambda7", "m12_2", "m11_2", "m22_2", \
                                              "Lambda1", "Lambda2", "Lambda3", "Lambda4", "Lambda5", "Lambda6", "Lambda7", "M12_2", "M11_2", "M22_2", \
                                              "m_h", "m_H", "m_A", "m_Hp", "tanb", "sba","alpha"};
          // fill entries in basis
@@ -666,6 +666,7 @@ namespace Gambit
          double ba = 0.5*acos(c2ba);
          basis["alpha"] = beta - ba;
          // else
+         basis["sba"] = sin(beta - alpha);
          // input_basis["alpha"] = beta - asin(input_basis["sba"]);
 
          print_THDM_spectrum(basis);
