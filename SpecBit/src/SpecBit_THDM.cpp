e326c580b520062e5d41beb06d1bc7bd1be09470
Filip Rajec <filiprajec@yahoo.com>
2019-04-26 18:18:44 +0930

    Converted all likelihoods in SpecBit_THDM to use built in Stats routines

--- SpecBit/src/SpecBit_THDM.cpp
+++ SpecBit/src/SpecBit_THDM.cpp
@@ -649,79 +649,7 @@ namespace Gambit
     // Constraint helper functions
     // Necessary forward declaration
     double Z_w(void * params);
-
-    double get_chi(const double value, chi_options type, chi_options sign, const double value_bound, const double error) {
-      double chi = 0.0;
-      const bool use_one_sigma_bound = false;
-      double sigma_limit = 0.64, sigma_rescale = 0.6;
-      if (!use_one_sigma_bound) {
-        sigma_limit = 1.; sigma_rescale = 1.;
-      }
-
-      switch (type) {
-        case observable:
-          switch (sign) {
-                case less_than:
-                    if(value<value_bound) {
-                        return 0;
-                    }
-                    else {
-                        return Stats::gaussian_loglikelihood(value,value_bound,0.0,error,true);
-                        // pow(((value-value_bound)/error),2);
-                    }
-                break;
-                case greater_than:
-                    if(value>value_bound) {
-                        return 0;
-                        }
-                    else {
-                        return Stats::gaussian_loglikelihood(value,value_bound,0.0,error,true);
-                        //pow(((value-value_bound)/error),2);
-                    }
-                break;
-            case distance_from:
-              chi = Stats::gaussian_loglikelihood(value,value_bound,0.0,error,true);
-              //pow(((value-value_bound)/error),2);
-                break;
-            default:
-              return chi;
-            }
-          break;
-        case bound:
-          switch (sign)
-          {
-            case less_than:
-              if(value<(value_bound/sigma_limit)) {
-                return 0;
-              }
-              else {
-                return Stats::gaussian_loglikelihood(value,value_bound,0.0,error,true);
-                //pow(((sigma_rescale*value)/(value_bound)-1.0),2);
-              }
-            break;
-            case greater_than:
-              if(value>(value_bound/sigma_limit)) {
-                return 0;
-              }
-              else {
-                return Stats::gaussian_loglikelihood(value,value_bound,0.0,error,true);
-                //pow(((sigma_rescale*value)/(value_bound)-1.0),2);
-              }
-            break;
-            case distance_from:
-              return Stats::gaussian_loglikelihood(value,value_bound,0.0,error,true);
-              //pow(((sigma_rescale*value)/(value_bound)-1.0),2);
-            break;
-            default:
-              return chi;
-          }
-          break;
-        default:
-          return chi;
-      }
-      return chi;
-    }
-
+    
     // void get_CKM_from_Wolfenstein_parameters(complex<double> CKM[2][2], double lambda, double A, double rho, double eta) {
     //   std::complex<double> i_eta(0, eta);
     //   CKM[0][0] = 1 - pow(lambda,2)/2;
@@ -1970,7 +1898,7 @@ namespace Gambit
       //calculate the total error of each point
       double chi2 = 0.0;
       for (auto const& eachEig : abs_eigenvalues) {
-            chi2 += get_chi(eachEig,bound,less_than,unitarity_upper_limit,sigma);
+          chi2 += Stats::gaussian_lower_limit(eachEig,unitarity_upper_limit,0.0,sigma,false);
       }
       if (print_debug_checkpoints) cout << "Checkpoint: 29A " << endl;
       return chi2;
@@ -2077,7 +2005,7 @@ namespace Gambit
         a01_even_minus, a01_odd_plus, a01_odd_minus, a10_odd, a11_even_plus, a11_even_minus, a11_odd};
 
       for(auto const& eig: eigenvalues) {
-        chi2 += get_chi((abs(eig-i/2.0)),bound,less_than,unitarity_upper_limit,sigma)*pow(10,0);
+        chi2 += Stats::gaussian_lower_limit((abs(eig-i/2.0)),unitarity_upper_limit,0.0,sigma,false);
       }
 
       #ifdef SPECBIT_DEBUG
@@ -2085,7 +2013,7 @@ namespace Gambit
           "a01_even_minus","a01_odd_plus","a01_odd_minus","a10_odd","a11_even_plus","a11_even_minus","a11_odd"};
         for(unsigned j=0; j < eigenvalues.size(); j++) {
             std::cout  <<  eigenvalue_keys[j] << " | " << eigenvalues[j] << " | " << abs(eigenvalues[j]-i/2.0) << \
-            " | chi^2 = " << get_chi((abs(eigenvalues[j]-i/2.0)),bound,less_than,unitarity_upper_limit,sigma) << std::endl;
+            " | chi^2 = " << Stats::gaussian_lower_limit((abs(eig-i/2.0)),unitarity_upper_limit,0.0,sigma,false) << std::endl;
         }
       #endif
 
@@ -2105,7 +2033,7 @@ namespace Gambit
       std::vector<double> lambda = get_lambdas_from_spectrum(container);
       // loop over all lambdas
       for(auto const& each_lambda: lambda) {
-        chi_2 += get_chi(abs(each_lambda),bound,less_than,perturbativity_upper_limit,sigma);
+        chi_2 += Stats::gaussian_lower_limit(abs(each_lambda),perturbativity_upper_limit,0.0,sigma,false)
         }
       return chi_2;
     }
@@ -2129,7 +2057,7 @@ namespace Gambit
             for (int p4=1;p4<7;p4++) {
                 if (p1 != 4 && p2 != 4 && p3 != 4 && p4 != 4){
                   hhhh_coupling = get_quartic_coupling(container,(particle_type)p1,(particle_type)p2,(particle_type)p3,(particle_type)p4);
-                  chi_2 += get_chi(abs(hhhh_coupling),bound,less_than,perturbativity_upper_limit,sigma);
+                  chi_2 += Stats::gaussian_lower_limit(abs(hhhh_coupling),perturbativity_upper_limit,0.0,sigma,false);
                   // if (abs(hhhh_coupling) > perturbativity_upper_limit) chi_2_gaussian += Stats::gaussian_loglikelihood(abs(hhhh_coupling),perturbativity_upper_limit,0.0,sigma,true);
                 }
             }
@@ -2154,20 +2082,20 @@ namespace Gambit
 
         double chi_2 = 0;
         const double sigma = 1.;
-        //observable likelihood used as this should be covered by prior and has a central value of zero
-        chi_2 += get_chi(lambda[1],observable,greater_than,0,sigma);
-        chi_2 += get_chi(lambda[2],observable,greater_than,0,sigma);
+
+        chi_2 += Stats::gaussian_upper_limit(lambda[1],0.0,0.0,sigma,false);
+        chi_2 += Stats::gaussian_upper_limit(lambda[2],0.0,0.0,sigma,false);
 
         if (std::isnan(sqrt(lambda[1]*lambda[2]))) {
             chi_2 = -L_MAX;
         }
         else {
-            chi_2 += get_chi(lambda[3],bound,greater_than,-sqrt(lambda[1]*lambda[2]),sigma);
+            chi_2 += Stats::gaussian_upper_limit(lambda[3],-sqrt(lambda[1]*lambda[2]),0.0,sigma,false);
             if (lambda[6] == 0.0 || lambda[7]==0.0) {
-              chi_2 += get_chi(lambda[3]+lambda[4]-abs(lambda[5]),observable, greater_than, -sqrt(lambda[1]*lambda[2]),sigma);
+              chi_2 += Stats::gaussian_upper_limit(lambda[3]+lambda[4]-abs(lambda[5]),-sqrt(lambda[1]*lambda[2]),0.0,sigma,false);
             }
             else {
-              chi_2 += get_chi(lambda[3]+lambda[4]-lambda[5],observable, greater_than, -sqrt(lambda[1]*lambda[2]),sigma);
+              chi_2 += Stats::gaussian_upper_limit(lambda[3]+lambda[4]-lambda[5],-sqrt(lambda[1]*lambda[2]),0.0,sigma,false);
             }
         }
         return chi_2;
@@ -2183,7 +2111,7 @@ namespace Gambit
       const double sigma = 1.;
       //-----------------------------
       // chi2 function
-      return -1.0*get_chi((1.0 - sba),bound,less_than,sba_tolerance,sigma);
+      return Stats::gaussian_lower_limit((1.0 - sba),sba_tolerance,0.0,sigma,false);
     }
 
 
@@ -2260,8 +2188,8 @@ namespace Gambit
       double chi2 = 0.0;
 
         // calculate chi2 * observable used due to zero being limit 
-      chi2 += get_chi(discriminant.real(),observable,greater_than,0.0,sigma);
-      chi2 += get_chi(discriminant.imag(),observable,greater_than,0.0,sigma);
+      chi2 += Stats::gaussian_upper_limit(discriminant.real(),-sqrt(lambda[1]*lambda[2]),0.0,sigma,false); 
+      chi2 += Stats::gaussian_upper_limit(discriminant.imag(),-sqrt(lambda[1]*lambda[2]),0.0,sigma,false); 
 
       // check for NaN - should *not* happen but has crashed scans before. Most probable culprit is k when lambda_2 = 0. TODO: find workaround
       if (std::isnan(chi2)) {
