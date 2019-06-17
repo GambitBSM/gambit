9035a71c5729287418c4c4bab372d0ebc4639e17
Filip Rajec <filiprajec@yahoo.com>
2019-06-17 22:39:50 +0930

    fixed string->char* bug (2)

--- Backends/src/frontends/HiggsSignals_1_4.cpp
+++ Backends/src/frontends/HiggsSignals_1_4.cpp
@@ -48,8 +48,8 @@ BE_INI_FUNCTION
     }
 
     // initialize HiggsSignals with the latest results (or custom experimental table) and set pdf shape
-    std::string Expt_string = runOptions->getValueOrDef<std::string>("latestresults", "HS_Expt_string");
-    initialize_HiggsSignals(nHneut,nHplus,Expt_string.c_str());
+    const char* Expt_string = runOptions->getValueOrDef<char*>("latestresults", "HS_Expt_string");
+    initialize_HiggsSignals(nHneut,nHplus,Expt_string);
     setup_pdf(pdf);
 
     for (auto it = mylocks.begin(); it != mylocks.end(); ++it)
