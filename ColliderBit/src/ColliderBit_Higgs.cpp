7645da92d7e0f401067963519f015ec5fccb481f
Filip Rajec <filiprajec@yahoo.com>
2019-07-30 19:13:28 +0930

    debug commit

--- ColliderBit/src/ColliderBit_Higgs.cpp
+++ ColliderBit/src/ColliderBit_Higgs.cpp
@@ -1064,7 +1064,7 @@ namespace Gambit
           std::cout << "h total width: " << j << " | " << THDM_total_widths.gamma_tot_h[j] << std::endl;	 
           for (int i=1;i<=3;i++) {
             result.BR_hjhihi[i-1][j-1]=THDM_decay_widths.gamma_hhh[j][i][i]/result.hGammaTot[j-1];
-            std::cout << "gamma hhh: (" << j << i << i << "): "<< result.BR_hjhihi[i-1][j-1] << std::endl;
+            std::cout << "gamma hhh: (" << j << i << i << "): "<< THDM_decay_widths.gamma_hhh[j][i][i] << std::endl;
             c = THDM_couplings.vvh[2][j][i];
             result.ghjhiZ[i-1][j-1]=pow(abs(c)/(g/2./costw),2);
             if (debug) printf("%2d %2d hihjZ %16.8E\n", j, i, result.ghjhiZ[i-1][j-1]);
