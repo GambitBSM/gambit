20fbd46b09f7cd58feddf7e91081d15125fd5cc7
Filip Rajec <filiprajec@yahoo.com>
2019-07-30 11:25:36 +0930

    debug commit

--- SpecBit/src/SpecBit_THDM.cpp
+++ SpecBit/src/SpecBit_THDM.cpp
@@ -2302,8 +2302,10 @@ namespace Gambit
          break; 
          case HB_effc_couplings:
             // just through and execute next case statement as they have the same input
+            std::cout << "purpose: non-SM" << std::endl;
          case HB_effc_SM_like_couplings:
             // fill neutral scalar coupling
+            std::cout << "begin coupling fill" << std::endl;
             for (int h=1; h<4; h++) { 
               container.THDM_object->get_coupling_hdd(h,2,2,couplings.hdd_cs[h][2][2],couplings.hdd_cp[h][2][2]);
               container.THDM_object->get_coupling_hdd(h,3,3,couplings.hdd_cs[h][3][3],couplings.hdd_cp[h][3][3]);
@@ -2321,13 +2323,13 @@ namespace Gambit
               check_nan(couplings.hll_cs[h][3][3], "hll coupling "+std::to_string(h)); check_nan(couplings.hll_cp[h][3][3], "hll coupling "+std::to_string(h));
               check_nan(couplings.vvh[2][2][h], "vvh coupling 22"+std::to_string(h));
               check_nan(couplings.vvh[3][3][h], "vvh coupling 33"+std::to_string(h));
-              std::cout << "(SpecBit) get alpha: " << container.THDM_object->get_alpha() << container.THDM_object->get_sba() << std::endl;
-              container.THDM_object->print_param_phys();
-              std::cout << "(SpecBit) DEBUG hdd (" << h << "33): "<< couplings.hdd_cs[h][3][3] << " " << couplings.hdd_cp[h][3][3] << std::endl;
-            double kd1, kd2, kd3;
-            container.THDM_object->get_kappa_down(kd1,kd2,kd3);
-            std::cout << "dmass p 3: " << container.THDM_object->get_SM().get_dmass_pole(3) << std::endl;
-            std::cout << "kappa down: " << kd1 << " " << kd2 << " " << kd3 << std::endl;
+            // std::cout << "(SpecBit) get alpha: " << container.THDM_object->get_alpha() << container.THDM_object->get_sba() << std::endl;
+            // container.THDM_object->print_param_phys();
+            std::cout << "(SpecBit) DEBUG hdd (" << h << "133): "<< couplings.hdd_cs[1][3][3] << " " << couplings.hdd_cp[1][3][3] << std::endl;
+            // double kd1, kd2, kd3;
+            // container.THDM_object->get_kappa_down(kd1,kd2,kd3);
+            // std::cout << "dmass p 3: " << container.THDM_object->get_SM().get_dmass_pole(3) << std::endl;
+            // std::cout << "kappa down: " << kd1 << " " << kd2 << " " << kd3 << std::endl;
             }
          break;
       }
