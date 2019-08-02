64f1d0be88df0a01efe2b31f3c391a23989a8737
Filip Rajec <filiprajec@yahoo.com>
2019-08-02 20:46:41 +0930

    removed debug message from specbit

--- SpecBit/src/SpecBit_THDM.cpp
+++ SpecBit/src/SpecBit_THDM.cpp
@@ -659,7 +659,6 @@ namespace Gambit
       set_SM(SM,sminputs,THDM_object);
       THDM_object->set_param_sm(m_h);
       THDM_object->set_yukawas_type(1);
-      THDM_object->print_param_gen();
     }
 
     struct THDM_spectrum_container {
