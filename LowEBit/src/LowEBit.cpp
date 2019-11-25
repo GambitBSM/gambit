ba5bf8d87a950c10d79c2471c11daeb42fa62e69
Diman1992 <d.skodras@gmx.de>
2019-11-25 17:07:12 +0100

    added mixing matrices for qCEDM expressions

--- LowEBit/src/LowEBit.cpp
+++ LowEBit/src/LowEBit.cpp
@@ -32,19 +32,92 @@ namespace Gambit
       {
           using namespace Pipes::CPV_Wilson_q_Simple;
 
-          result.Cu[1] = (*Param["kappaU"])*(*Param["SinPhiU"])
-             *1.16E-4;
-          result.Cd[1] = (*Param["kappaD"])*(*Param["SinPhiD"])
-             *1.45E-4;
-          result.Cs[1] = (*Param["kappaS"])*(*Param["SinPhiS"])
-             *1.45E-4;
-          result.Cu[2] = (*Param["kappaU"])*(*Param["SinPhiU"])
-             *(-1.07E-4);
-          result.Cd[2] = (*Param["kappaD"])*(*Param["SinPhiD"])
-             *(-1.14E-4);
-          result.Cs[2] = (*Param["kappaS"])*(*Param["SinPhiS"])
-             *(-1.14E-4);
-      }
+		  double kappaS[6] = {
+			  (*Param["kappaU"])*(*Param["SinPhiU"]),
+			  (*Param["kappaD"])*(*Param["SinPhiD"]),
+			  (*Param["kappaS"])*(*Param["SinPhiS"]),
+			  (*Param["kappaC"])*(*Param["SinPhiC"]),
+			  (*Param["kappaB"])*(*Param["SinPhiB"]),
+			  (*Param["kappaT"])*(*Param["SinPhiT"])};
+		  double kappaC[6] = {
+			  (*Param["kappaU"])*(pow(1-pow(*Param["SinPhiU"],2),0.5)),
+			  (*Param["kappaD"])*(pow(1-pow(*Param["SinPhiD"],2),0.5)),
+			  (*Param["kappaS"])*(pow(1-pow(*Param["SinPhiS"],2),0.5)),
+			  (*Param["kappaC"])*(pow(1-pow(*Param["SinPhiC"],2),0.5)),
+			  (*Param["kappaB"])*(pow(1-pow(*Param["SinPhiB"],2),0.5)),
+			  (*Param["kappaT"])*(pow(1-pow(*Param["SinPhiT"],2),0.5))
+			  };
+
+		//MqX and Bs are from draft with orders <10^-9 omitted
+		  double Mu3[6][6] = {
+			  {2.58E-7,1.76E-7,6.96E-5,8.06E-3,1.12E-1,2.7},
+			  {2.02E-7,0      ,0      ,0      ,0      ,0  },
+			  {8.03E-5,0      ,0      ,3.0E-8 ,6.57E-8,0  },
+			  {9.31E-3,0      ,3.0E-8 ,-2.04E-5,2.5E-5,0  },
+			  {1.37E-1,0      ,6.57E-9,2.5E-5,-1.68E-4,0  },
+			  {3.94   ,0      ,0      ,0      ,0  ,3.04E-2}};
+		  double Md3[6][6] = {
+			  {0      ,5.00E-8,0      ,0      ,0      ,0  },
+			  {4.42E-8,1.21E-6,5.73E-5,1.65E-2,1.02E-1,2.38},
+			  {0      ,6.80E-5,0      ,3.0E-8 ,6.57E-8,0  },
+			  {0      ,1.77E-2,3.0E-8 ,-2.04E-5,2.5E-5,0  },
+			  {0      ,1.21E-1,6.57E-9,2.5E-5,-1.68E-4,0  },
+			  {0      ,3.94E-0,0      ,0      ,0  ,3.04E-2}};
+		  double Ms3[6][6] = {
+			  {0      ,0      ,5.00E-8,0      ,0      ,0  },
+			  {0      ,0      ,1.71E-7,0      ,0      ,0  },
+			  {4.42E-8,1.44E-7,4.79E-4,1.65E-2,1.02E-1,2.38},
+			  {0      ,0      ,1.77E-2,-2.04E-5,2.5E-5,0  },
+			  {0      ,0      ,1.27E-1,2.5E-5,-1.68E-4,0  },
+			  {0      ,0      ,3.94E-0,0      ,0  ,3.04E-2}};
+		  double Mu4[6][6] = {
+			  {5.66E-7 ,-5.60E-7,-2.22E-4,-5.68E-2 ,-2.59E-1,-7.14},
+			  {-6.71E-7,0       ,0        ,1.02E-9 ,0       ,0  },
+			  {-2.66E-4,0       ,0        ,4.03E-7 ,-7.24E-8,0  },
+			  {-6.66E-2,1.02E-9 ,4.03E-7  ,-2.74E-4,3.13E-4 ,0  },
+			  {-3.28E-1,0       ,-7.24E-8 ,3.13E-4 ,1.85E-3 ,0  },
+			  {-9.94   ,0       ,0        ,0       ,0       ,-9.62E-2}};
+		  double Md4[6][6] = {
+			  {0       ,-1.44E-7,0       ,0       ,0       ,0  },
+			  {-1.12E-7,2.64E-6 ,-2.22E-4,-5.68E-2,-2.59E-1,-7.14},
+			  {0       ,-2.66E-4,0       ,4.03E-7 ,-7.24E-8,0  },
+			  {0       ,-6.66E-2,4.03E-7 ,-2.74E-4,3.13E-4 ,0  },
+			  {0       ,-2.28E-1,-7.24E-8,3.13E-4 ,1.85E-3 ,0  },
+			  {0       ,-9.94E-0,0       ,0       ,0       ,-9.62E-2}};
+		  double Ms4[6][6] = {
+			  {0       ,0       ,-1.44E-7,0       ,0       ,0  },
+			  {0       ,0       ,-6.71E-7,0       ,0       ,0  },
+			  {-1.20E-7,-5.60E-7,1.05E-3,-5.68E-2 ,-2.59E-1,-7.14},
+			  {0       ,1.02E-9 ,-6.66E-2,-2.74E-4,3.13E-4 ,0  },
+			  {0       ,0       ,-3.28E-1,3.13E-4 ,-1.85E-3,0  },
+			  {0       ,0       ,-9.94E-0,0       ,0       ,-9.62E-2}};
+		  double Mw[6][6] = {
+			  {0      ,0       ,0      ,2.72E-9 ,0       ,0  },
+			  {0      ,0       ,0      ,1.27E-8 ,1.62E-9 ,0  },
+			  {0      ,0       ,0      ,5.05E-6 ,6.41E-7 ,0  },
+			  {2.72E-9,1.27E-8 ,5.05E-6,-3.43E-3,4.14E-3 ,0  },
+			  {0      ,1.62E-9 ,6.41E-7,4.14E-3 ,-1.64E-2,0  },
+			  {0      ,0       ,0      ,0       ,0       ,-2.01E-1}};
+		  double Bu3[6] = {3.42,0,0,0,0,0};
+		  double Bd3[6] = {0,2.38,0,0,0,0};
+		  double Bs3[6] = {0,0,2.38,0,0,0};
+		  double Bu4[6] = {1.31,0,0,0,0,0};
+		  double Bd4[6] = {0,0.936,0,0,0,0};
+		  double Bs4[6] = {0,0,0.936,0,0,0};
+
+		  for(int i = 0; i<6; i++){
+			  for(int j = 0; j<6; j++){
+		          result.Cu[1] = result.Cu[1] + kappaS[i]*Mu3[i][j]*kappaC[j] + kappaS[i]*Bu3[i];
+		          result.Cd[1] = result.Cd[1] + kappaS[i]*Md3[i][j]*kappaC[j] + kappaS[i]*Bd3[i];
+		          result.Cs[1] = result.Cs[1] + kappaS[i]*Ms3[i][j]*kappaC[j] + kappaS[i]*Bs3[i];
+		          result.Cu[2] = result.Cu[2] + kappaS[i]*Mu4[i][j]*kappaC[j] + kappaS[i]*Bu4[i];
+		          result.Cd[2] = result.Cd[2] + kappaS[i]*Md4[i][j]*kappaC[j] + kappaS[i]*Bd4[i];
+		          result.Cs[2] = result.Cs[2] + kappaS[i]*Ms4[i][j]*kappaC[j] + kappaS[i]*Bs4[i];
+				  result.Cw[1] = result.Cw[1] + kappaS[i]*Mw[i][j]*kappaC[j];
+			  }
+		  }
+	  }
+			  
 
       void EDM_q_Wilson(dq &result)
 	  // Calculation of quark EDMs (at mu_had) from Wilson Coefficients in e cm
