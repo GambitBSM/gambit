ca9f37968278161c3bcbbb97e6ff584e9f44eb75
filiprajec <filip.rajec@adelaide.edu.au>
2020-07-22 13:56:37 +0000

    backends cmake default version bug fixed

--- cmake/backends.cmake
+++ cmake/backends.cmake
@@ -1060,7 +1060,6 @@ if(NOT ditched_${name}_${ver})
     INSTALL_COMMAND ""
   )
   add_extra_targets("backend" ${name} ${ver} ${dir} ${dl} clean)
-  set_as_default_version("backend" ${name} ${ver})
 endif()
 
 # HiggsSignals 2.4.0
