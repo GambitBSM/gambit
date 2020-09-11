85e2320f1b05ad83ecc0d1d3be505c2062c80752
Tomas Gonzalo <tomas.gonzalo@monash.edu>
2020-09-11 12:22:51 +1000

    Changed 2020 LHCb file name

--- FlavBit/src/FlavBit.cpp
+++ FlavBit/src/FlavBit.cpp
@@ -3246,7 +3246,7 @@ namespace Gambit
     void HEPLike_B2KstarmumuAng_LogLikelihood_LHCb_2020(double &result)
     {
       using namespace Pipes::HEPLike_B2KstarmumuAng_LogLikelihood_LHCb_2020;
-      static const std::string inputfile = path_to_latest_heplike_data() + "/data/LHCb/RD/Bd2KstarMuMu_Angular/NAME_q2_";
+      static const std::string inputfile = path_to_latest_heplike_data() + "/data/LHCb/RD/Bd2KstarMuMu_Angular/CERN-EP-2020-027_q2_";
       static std::vector<str> obs_list = runOptions->getValue<std::vector<str>>("obs_list");
       static std::vector<HepLike_default::HL_nDimGaussian> nDimGaussian = {
         HepLike_default::HL_nDimGaussian(inputfile + "0.1_0.98.yaml"),
