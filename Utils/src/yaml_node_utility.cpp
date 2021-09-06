18ea8c82747e6755809dea2056aab49d852f3a86
Tomek Procter <tprocter46@gmail.com>
2021-09-06 11:48:37 +0100

    Removed commented out old code

--- Utils/src/yaml_node_utility.cpp
+++ Utils/src/yaml_node_utility.cpp
@@ -30,45 +30,10 @@ namespace Gambit
       std::smatch match;
       while ( std::regex_search( text, match, env ) ) {
       const char * s = getenv( match[1].str().c_str() );
-      const std::string var( s == NULL ? "" : s );
-      //text.replace( match[0].first, match[0].second(), var ); //< some problem with RH devtoolsets?                
-      text.replace( match[0].first - std::cbegin(text), match[0].str().size(), var ); //< workaround :-/
+      const std::string var( s == NULL ? "" : s );              
+      text.replace( match[0].first - std::cbegin(text), match[0].str().size(), var );
       }
     }
-    ////OLD VERSION BELOW
-    /// Expand environment variables in the given string.
-    // void NodeUtility::autoExpandEnvironmentVariables(std::string & text)
-    // {
-    //   // C++ regex does not support negative lookahead. So let us reverse the string.
-    //   std::reverse(text.begin(), text.end());
-
-    //   // Matches reverse ${MYVAR}, but not \${MYVAR} and $MYVAR
-    //   const static std::regex env( "\\}([^{]+)\\{\\$(?!\\\\)" );
-
-    //   std::smatch match;
-    //   while (std::regex_search(text, match, env))
-    //   {
-    //       // Reverse the found match into a temporary variable, this is what we actually
-    //       // want to look for, e.g. we found RAV, but we want to look for VAR.
-    //       std::string tmp = match[1].str();
-    //       std::reverse(tmp.begin(), tmp.end());
-
-    //       // Look for VAR
-    //       const char * s = std::getenv(tmp.c_str());
-
-    //       std::string var(s == NULL ? "" : s);
-    //       if (s == NULL)
-    //       {
-    //         std::cout << "Environment variable " << match.str() << " not set";
-    //       }
-    //       // Reverse the found environment variable...
-    //       std::reverse(var.begin(), var.end());
-    //       // and plug it into the text string.
-    //       text.replace(match[0].first, match[0].second, var);
-    //   }
-    //   // Finally return the text string in the normal order.
-    //   std::reverse(text.begin(), text.end());
-    // }
 
     /// Remove characters in the given string.
     void NodeUtility::removeCharsFromString(std::string& text, const char* charsToRemove)
