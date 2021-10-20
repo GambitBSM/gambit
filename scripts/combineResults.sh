2aa566d52cf075463b2959b558dee1067d349a27
mjw63 <martin.white@adelaide.edu.au>
2021-10-21 03:29:49 +1030

    fix combineResults

--- scripts/combineResults.sh
+++ scripts/combineResults.sh
@@ -5,6 +5,6 @@ NO_COL=" \033[0m\n${HEADING}\n\n"
 # echo -e "${BLUE}MAKE SURE YOU HAVE py2 ACTIVATED${NO_COL}"
 echo -e "${BLUE}combining results into runs/combined.hdf5${NO_COL}"
 
-cd ../work/runs/samples && \
-echo "python2 ../../../Printers/scripts/combine_hdf5.py ../combined.hdf5 /data `ls | sed ':a;N;$!ba;s/\n/ /g'`" > ../combine.sh && \
+cd ../runs/samples && \
+echo "python ../../Printers/scripts/combine_hdf5.py ../combined.hdf5 /data `ls | sed ':a;N;$!ba;s/\n/ /g'`" > ../combine.sh && \
 bash ../combine.sh
