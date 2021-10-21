96b5c30eced92b230da7c5a5c0b36201dc3d9d99
Alexander Woodcock <aw1259@gadi-login-05.gadi.nci.org.au>
2021-10-21 11:57:25 +1100

    minor changes

--- scripts/runScan.sh
+++ scripts/runScan.sh
@@ -6,6 +6,26 @@ echo -e "${BLUE}running gambit scan${NO_COL}"
 export OMP_NUM_THREADS=1
 # export slots=32
 (mkdir ../work || true) && \
-cd ../work && \
-rm -rf ./* && \
-mpiexec --oversubscribe -np 32 ../gambit -rf ../yaml_files/THDMI.yaml > out.txt
+cd .. && \
+rm -rf work/*
+
+# won't work on Gadi
+cd work && \
+mpiexec -np 48 ../gambit -rf ../yaml_files/THDMI.yaml > out.txt
+
+# export I_MPI_DEBUG=5 && \
+# export OMP_NUM_THREADS=1 && \
+# export I_MPI_HYDRA_BRANCH_COUNT=$(($PBS_NCPUS / 48)) && \
+# export HOSTFILE=hosts_${PBS_JOBID}.txt && \
+# uniq < ${PBS_NODEFILE} > ${HOSTFILE} && \
+# export CPN=$( grep -c ${HOSTNAME} ${PBS_NODEFILE} ) && \
+# cd work
+
+# # Intel MPI command
+# mpirun -np $(( ${PBS_NCPUS}/${OMP_NUM_THREADS} )) -ppn $(( ${CPN}/${OMP_NUM_THREADS} )) -f ../${HOSTFILE} ../gambit -f ../yaml_files/THDMI.yaml
+
+# # OpenMPI command
+# # mpirun -np -npernode $(( ${CPN}/${OMP_NUM_THREADS} )) -hostfile ${HOSTFILE} -output-filename stdout ${GDIR}/gambit -f ${GDIR}/${YAMLF}
+
+# # -outfile-pattern=stdout/out-%r -errfile-pattern=stderr/err-%r
+# rm ../${HOSTFILE}
