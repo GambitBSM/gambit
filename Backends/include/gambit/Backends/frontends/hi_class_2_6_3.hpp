6675428692eba93d79968675cc43f57fadf32b9b
ChrisJChang <christopher.chang@uqconnect.edu.au>
2021-11-23 11:54:48 +1000

    Fix Function Naming in Hi_class

--- Backends/include/gambit/Backends/frontends/hi_class_2_6_3.hpp
+++ Backends/include/gambit/Backends/frontends/hi_class_2_6_3.hpp
@@ -23,30 +23,30 @@ LOAD_LIBRARY
 
 #ifdef HAVE_PYBIND11
 
-  BE_CONV_FUNCTION(get_classy_cosmo_object, pybind11::object, (), "get_classy_cosmo_object")
-  BE_CONV_FUNCTION(get_classy_backendDir, std::string, (), "get_classy_backendDir")
+  BE_CONV_FUNCTION(get_hi_class_cosmo_object, pybind11::object, (), "get_classy_cosmo_object")
+  BE_CONV_FUNCTION(get_hi_class_backendDir, std::string, (), "get_classy_backendDir")
 
-  BE_CONV_FUNCTION(class_get_rs,        double, (), "class_get_rs")
-  BE_CONV_FUNCTION(class_get_tau_reio,  double, (), "class_get_tau_reio")
-  BE_CONV_FUNCTION(class_get_z_reio,    double, (), "class_get_z_reio")
-  BE_CONV_FUNCTION(class_get_Neff,      double, (), "class_get_Neff")
-  BE_CONV_FUNCTION(class_get_sigma8,      double, (), "class_get_sigma8")
-  BE_CONV_FUNCTION(class_get_Omega0_r,    double, (), "class_get_Omega0_r")
-  BE_CONV_FUNCTION(class_get_Omega0_ur,     double, (), "class_get_Omega0_ur")
-  BE_CONV_FUNCTION(class_get_Omega0_m,    double, (), "class_get_Omega0_m")
-  BE_CONV_FUNCTION(class_get_Omega0_ncdm_tot, double, (), "class_get_Omega0_ncdm_tot")
-  BE_CONV_FUNCTION(class_get_Omega0_Lambda,   double, (), "class_get_Omega0_Lambda")
-  BE_CONV_FUNCTION(class_get_H0,              double, (), "class_get_H0")
-  BE_CONV_FUNCTION(class_get_Da, double, (double), "class_get_Da")
-  BE_CONV_FUNCTION(class_get_Dl, double, (double), "class_get_Dl")
-  BE_CONV_FUNCTION(class_get_Hz, double, (double), "class_get_Hz")
+  BE_CONV_FUNCTION(hi_class_get_rs,        double, (), "class_get_rs")
+  BE_CONV_FUNCTION(hi_class_get_tau_reio,  double, (), "class_get_tau_reio")
+  BE_CONV_FUNCTION(hi_class_get_z_reio,    double, (), "class_get_z_reio")
+  BE_CONV_FUNCTION(hi_class_get_Neff,      double, (), "class_get_Neff")
+  BE_CONV_FUNCTION(hi_class_get_sigma8,      double, (), "class_get_sigma8")
+  BE_CONV_FUNCTION(hi_class_get_Omega0_r,    double, (), "class_get_Omega0_r")
+  BE_CONV_FUNCTION(hi_class_get_Omega0_ur,     double, (), "class_get_Omega0_ur")
+  BE_CONV_FUNCTION(hi_class_get_Omega0_m,    double, (), "class_get_Omega0_m")
+  BE_CONV_FUNCTION(hi_class_get_Omega0_ncdm_tot, double, (), "class_get_Omega0_ncdm_tot")
+  BE_CONV_FUNCTION(hi_class_get_Omega0_Lambda,   double, (), "class_get_Omega0_Lambda")
+  BE_CONV_FUNCTION(hi_class_get_H0,              double, (), "class_get_H0")
+  BE_CONV_FUNCTION(hi_class_get_Da, double, (double), "class_get_Da")
+  BE_CONV_FUNCTION(hi_class_get_Dl, double, (double), "class_get_Dl")
+  BE_CONV_FUNCTION(hi_class_get_Hz, double, (double), "class_get_Hz")
 
-  BE_CONV_FUNCTION(class_get_lensed_cl, std::vector<double>, (str), "class_get_lensed_cl")
-  BE_CONV_FUNCTION(class_get_unlensed_cl, std::vector<double>, (str), "class_get_unlensed_cl")
-  BE_CONV_FUNCTION(class_get_scale_independent_growth_factor,   double, (double), "class_get_scale_independent_growth_factor")
-  BE_CONV_FUNCTION(class_get_scale_independent_growth_factor_f, double, (double), "class_get_scale_independent_growth_factor_f")
+  BE_CONV_FUNCTION(hi_class_get_lensed_cl, std::vector<double>, (str), "class_get_lensed_cl")
+  BE_CONV_FUNCTION(hi_class_get_unlensed_cl, std::vector<double>, (str), "class_get_unlensed_cl")
+  BE_CONV_FUNCTION(hi_class_get_scale_independent_growth_factor,   double, (double), "class_get_scale_independent_growth_factor")
+  BE_CONV_FUNCTION(hi_class_get_scale_independent_growth_factor_f, double, (double), "class_get_scale_independent_growth_factor_f")
 
-  BE_INI_DEPENDENCY(classy_input_params, Classy_input)
+  BE_INI_DEPENDENCY(hi_class_input_params, Classy_input)
 
 #endif
 
--- Backends/src/frontends/hi_class_2_6_3.cpp
+++ Backends/src/frontends/hi_class_2_6_3.cpp
@@ -24,7 +24,7 @@
 ///  *********************************************
 
 #include "gambit/Backends/frontend_macros.hpp"
-#include "gambit/Backends/frontends/hi_classy_2_6_3.hpp"
+#include "gambit/Backends/frontends/hi_class_2_6_3.hpp"
 
 #ifdef HAVE_PYBIND11
 
@@ -40,22 +40,22 @@
     pybind11::dict static prev_input_dict;
 
     // return cosmo object. Need to pass this to MontePython for Likelihoods calculations
-    pybind11::object get_classy_cosmo_object()
+    pybind11::object get_hi_class_cosmo_object()
     {
       return cosmo;
     }
 
     // return backend directory of CLASS
-    std::string get_classy_backendDir()
+    std::string get_hi_class_backendDir()
     {
       return backendDir;
     }
 
     /// test if two dictionaries contain exactly the same values for all keys
     /// return true if so, false if there is at least one different value
-    bool compare_dicts(pybind11::dict classy_input, pybind11::dict prev_input_dict)
+    bool compare_dicts(pybind11::dict hi_class_input, pybind11::dict prev_input_dict)
     {
-      for (auto it : classy_input)
+      for (auto it : hi_class_input)
       {
          // test if any pointer is being passed to CLASS -- if so you'd need to compare the contents
          // of the vectors, so just recompute by default in that case (atm the options that pass pointers
@@ -65,7 +65,7 @@
          if(it.first.cast<std::string>().find("pointer_to") != std::string::npos){return false;}
 
          // return false if unequal values are found
-         if (classy_input[it.first].cast<std::string>() != prev_input_dict[it.first].cast<std::string>()){return false;}
+         if (hi_class_input[it.first].cast<std::string>() != prev_input_dict[it.first].cast<std::string>()){return false;}
       }
       // all key values are identical --> no need to run class again, yay!
       return true;
@@ -74,7 +74,7 @@
     /// routine to check class input for consistency. If a case is not treated here class will
     /// throw an error saying that one input parameter was not read. Checking for some specific cases
     /// here allows us to give more informative error messages and fix suggestions
-    void class_input_consistency_checks(pybind11::dict classy_input)
+    void hi_class_input_consistency_checks(pybind11::dict hi_class_input)
     {
 
       std::ostringstream errMssg;
@@ -83,13 +83,13 @@
       // derive background quantities and an error occurs when trying to access quantities
       // depending on perturbations  =>
       // check if "modes" input is set while "output" is not set
-      if(classy_input.contains("modes") and not classy_input.contains("output"))
+      if(hi_class_input.contains("modes") and not hi_class_input.contains("output"))
       {
-        errMssg << "You are calling CLASS asking for the following modes to be computed : "<< pybind11::repr(classy_input["modes"]);
+        errMssg << "You are calling CLASS asking for the following modes to be computed : "<< pybind11::repr(hi_class_input["modes"]);
         errMssg << "\nHowever, you did not request any output that requires computing any perturbation spectra.\nHence CLASS";
         errMssg << " will not read the input 'modes' and won't run. Add the CLASS input parameter 'output' requesting";
-        errMssg << " a spectrum to be computed to the yaml file as run option, e.g. \n  - capability: classy_baseline_params\n";
-        errMssg << "    options:\n      classy_dict:\n        output: tCl";
+        errMssg << " a spectrum to be computed to the yaml file as run option, e.g. \n  - capability: hi_class_baseline_params\n";
+        errMssg << "    options:\n      hi_class_dict:\n        output: tCl";
         backend_error().raise(LOCAL_INFO,errMssg.str());
       }
     }
@@ -99,7 +99,7 @@
     // Boltzmann solver in use
 
     // get the lensed Cl.
-    std::vector<double> class_get_lensed_cl(std::string spectype)
+    std::vector<double> hi_class_get_lensed_cl(std::string spectype)
     {
       // Get dictionary containing all (lensed) Cl spectra
       pybind11::dict cl_dict = cosmo.attr("lensed_cl")();
@@ -121,7 +121,7 @@
     }
 
     // get the raw (unlensed) Cl.
-    std::vector<double> class_get_unlensed_cl(std::string spectype)
+    std::vector<double> hi_class_get_unlensed_cl(std::string spectype)
     {
       // Get dictionary containing the raw (unlensed) Cl spectra
       pybind11::dict cl_dict = cosmo.attr("raw_cl")();
@@ -143,14 +143,14 @@
     }
 
     // returns angular diameter distance for given redshift
-    double class_get_Da(double z)
+    double hi_class_get_Da(double z)
     {
       double Da = cosmo.attr("angular_distance")(z).cast<double>();
       return Da;
     }
 
     // returns luminosity diameter distance for given redshift
-    double class_get_Dl(double z)
+    double hi_class_get_Dl(double z)
     {
       double Dl = cosmo.attr("luminosity_distance")(z).cast<double>();
       return Dl;
@@ -158,7 +158,7 @@
 
     // returns scale_independent_growth_factor D(z) for CDM perturbations for a given
     // redshift (quantity defined by CLASS as index_bg_D in the background module)
-    double class_get_scale_independent_growth_factor(double z)
+    double hi_class_get_scale_independent_growth_factor(double z)
     {
       double growth_fact = cosmo.attr("scale_independent_growth_factor")(z).cast<double>();
       return growth_fact;
@@ -166,70 +166,70 @@
 
     // returns scale_independent_growth_factor_f f(z)=d ln D / d ln a for CDM perturbations
     // for given redshift (quantity defined by CLASS as index_bg_f in the background module)
-    double class_get_scale_independent_growth_factor_f(double z)
+    double hi_class_get_scale_independent_growth_factor_f(double z)
     {
       double growth_fact_f = cosmo.attr("scale_independent_growth_factor_f")(z).cast<double>();
       return growth_fact_f;
     }
 
     // returns Hubble parameter for given redshift
-    double class_get_Hz(double z)
+    double hi_class_get_Hz(double z)
     {
       double H_z = cosmo.attr("Hubble")(z).cast<double>();
       return H_z;
     }
 
     // returns Omega radiation today
-    double class_get_Omega0_r()
+    double hi_class_get_Omega0_r()
     {
       double Omega0_r = cosmo.attr("Omega_r")().cast<double>();
       return Omega0_r;
     }
 
     // returns Omega of ultra-relativistic species today
-    double class_get_Omega0_ur()
+    double hi_class_get_Omega0_ur()
     {
       double Omega0_ur = cosmo.attr("Omega_ur")().cast<double>();
       return Omega0_ur;
     }
 
     // returns Omega matter today
-    double class_get_Omega0_m()
+    double hi_class_get_Omega0_m()
     {
       double Omega0_m = cosmo.attr("Omega_m")().cast<double>();
       return Omega0_m;
     }
 
     // returns Omega ncdm today (contains contributions of all ncdm species)
-    double class_get_Omega0_ncdm_tot()
+    double hi_class_get_Omega0_ncdm_tot()
     {
       double Omega0_ncdm = cosmo.attr("Omega_ncdm_tot")().cast<double>();
       return Omega0_ncdm;
     }
 
     // returns Omega_Lambda
-    double class_get_Omega0_Lambda()
+    double hi_class_get_Omega0_Lambda()
     {
       double Omega0_Lambda = cosmo.attr("Omega_Lambda")().cast<double>();
       return Omega0_Lambda;
     }
 
     // returns sound horizon at drag epoch
-    double class_get_rs()
+    double hi_class_get_rs()
     {
       double rs_d = cosmo.attr("rs_drag")().cast<double>();
       return rs_d;
     }
 
     // returns optical depth at reionisation
-    double class_get_tau_reio()
+    double hi_class_get_tau_reio()
     {
       double rs_d = cosmo.attr("tau_reio")().cast<double>();
       return rs_d;
     }
 
     // returns redshift of reionisation
-    double class_get_z_reio()
+    double hi_class_get_z_reio()
     {
       double rs_d = cosmo.attr("z_reio")().cast<double>();
       return rs_d;
@@ -238,21 +238,21 @@
     // returns sigma8 at z = 0
     // (root mean square fluctuations density fluctuations within
     // spheres of radius 8/h Mpc)
-    double class_get_sigma8()
+    double hi_class_get_sigma8()
     {
       double sigma8 = cosmo.attr("sigma8")().cast<double>();
       return sigma8;
     }
 
     // returns Neff
-    double class_get_Neff()
+    double hi_class_get_Neff()
     {
       double Neff = cosmo.attr("Neff")().cast<double>();
       return Neff;
     }
 
     // Returns Hubble parameter today
-    double class_get_H0()
+    double hi_class_get_H0()
     {
       double H0 = cosmo.attr("Hubble")(0).cast<double>();
       return H0;
@@ -261,7 +261,7 @@
     // print primordial power spectrum for consistency check & debug purposes
     void print_pps()
     {
-      std::cout<< "Primordial spectrum from classy: "<< std::string(pybind11::str(cosmo.attr("get_primordial")())) << std::endl;
+      std::cout<< "Primordial spectrum from hi_class: "<< std::string(pybind11::str(cosmo.attr("get_primordial")())) << std::endl;
     }
 
   }
@@ -278,7 +278,7 @@ BE_INI_FUNCTION
     static int max_errors = 100;
 
     // get input for CLASS run set by CosmoBit
-    Classy_input input_container = *Dep::classy_input_params;
+    Classy_input input_container = *Dep::hi_class_input_params;
     pybind11::dict cosmo_input_dict = input_container.get_input_dict();
 
     // check if any energy injection-related parameters are contained in the
@@ -295,7 +295,7 @@ BE_INI_FUNCTION
       error << "\nwith exotic energy injection in the early Universe. However, you ";
       error << "\nare using a CLASS version that does not support these calculations. ";
       error << "\nGAMBIT will exit now. To fix this simply run";
-      error << "\n\ncd build; make nuke-classy_"<< STRINGIFY(VERSION) <<"; make classy_exo_2.7.2; cd ..";
+      error << "\n\ncd build; make nuke-hi_class_"<< STRINGIFY(VERSION) <<"; make hi_class; cd ..";
       backend_error().raise(LOCAL_INFO,error.str());
     }
 
@@ -304,9 +304,9 @@ BE_INI_FUNCTION
     if(first_run)
     {
       max_errors = runOptions->getValueOrDef<int>(100,"max_errors");
-      cosmo = classy.attr("Class")();
+      cosmo = hi_class.attr("Class")();
       // check input for consistency
-      class_input_consistency_checks(cosmo_input_dict);
+      hi_class_input_consistency_checks(cosmo_input_dict);
 
       // create copy of cosmo_input_dict to cache results from
       // previous run
@@ -327,7 +327,7 @@ BE_INI_FUNCTION
       cosmo.attr("empty")();
 
       // set cosmological parameters
-      logger() << LogTags::debug << "[classy_"<< STRINGIFY(VERSION) <<"] These are the inputs:"<<endl;
+      logger() << LogTags::debug << "[hi_class_"<< STRINGIFY(VERSION) <<"] These are the inputs:"<<endl;
       logger() << pybind11::repr(cosmo_input_dict) << EOM;
       cosmo.attr("set")(cosmo_input_dict);
 
@@ -338,19 +338,19 @@ BE_INI_FUNCTION
       //int recomputed = cosmo.attr("recomputed").cast<int>();
 
       // Try to run class and catch potential errors
-      logger() << LogTags::info << "[classy_"<< STRINGIFY(VERSION) <<"] Start to run \"cosmo.compute\"" << EOM;
+      logger() << LogTags::info << "[hi_class_"<< STRINGIFY(VERSION) <<"] Start to run \"cosmo.compute\"" << EOM;
       try
       {
-        // Try to run classy
+        // Try to run hi_class
         cosmo.attr("compute")();
         // reset counter when no exception is thrown.
         error_counter = 0;
-        logger() << LogTags::info << "[classy_"<< STRINGIFY(VERSION) <<"] \"cosmo.compute\" was successful" << EOM;
+        logger() << LogTags::info << "[hi_class_"<< STRINGIFY(VERSION) <<"] \"cosmo.compute\" was successful" << EOM;
       }
       catch (std::exception &e)
       {
         std::ostringstream errMssg;
-        errMssg << "Could not successfully execute cosmo.compute() in classy_"<< STRINGIFY(VERSION)<<"\n";
+        errMssg << "Could not successfully execute cosmo.compute() in hi_class_"<< STRINGIFY(VERSION)<<"\n";
         std::string rawErrMessage(e.what());
         // If the error is a CosmoSevereError raise an backend_error ...
         if (rawErrMessage.find("CosmoSevereError") != std::string::npos)
@@ -385,7 +385,7 @@ BE_INI_FUNCTION
         {
           errMssg << "Caught an unspecified error:"<<endl;
           errMssg << rawErrMessage;
-          cout << "An unspecified error occurred during compute() in classy_"<< STRINGIFY(VERSION) <<":\n";
+          cout << "An unspecified error occurred during compute() in hi_class_"<< STRINGIFY(VERSION) <<":\n";
           cout << rawErrMessage;
           cout << "\n(This point gets invalidated) " << endl;
           invalid_point().raise(errMssg.str());
@@ -398,7 +398,7 @@ BE_INI_FUNCTION
     // identical CLASS input -- skip compute step & save time!
     else
     {
-      logger() << LogTags::info << "[classy_"<< STRINGIFY(VERSION) <<"] \"cosmo.compute\" was skipped, input was identical to previously computed point" << EOM;
+      logger() << LogTags::info << "[hi_class_"<< STRINGIFY(VERSION) <<"] \"cosmo.compute\" was skipped, input was identical to previously computed point" << EOM;
       // CLASS did not recompute -> save this information in cosmo container, so MontePython
       // (and potentially other backends) has access to this information and can skip
       // their computations as well
