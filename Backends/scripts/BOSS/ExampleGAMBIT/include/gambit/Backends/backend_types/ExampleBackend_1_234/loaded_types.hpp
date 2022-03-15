af2baafa75830dd9b2590c91b59a8ce51ef3b053
Tomas Gonzalo <gonzalo@physik.rwth-aachen.de>
2022-03-15 18:56:41 +0100

    Fixed bug

--- Backends/scripts/BOSS/ExampleGAMBIT/include/gambit/Backends/backend_types/ExampleBackend_1_234/loaded_types.hpp
+++ Backends/scripts/BOSS/ExampleGAMBIT/include/gambit/Backends/backend_types/ExampleBackend_1_234/loaded_types.hpp
@@ -8,7 +8,6 @@
 // Indicate which types are provided by this backend, and what the symbols of their factories are.
 #define ExampleBackend_1_234_all_data \
   (( /*class*/(ClassThree),    /*constructors*/(("Factory_ClassThree_0_double__BOSS_1",())) )) \
-#define ExampleBackend_1_234_all_data \
   (( /*class*/(ClassFour),    /*constructors*/(("Factory_ClassFour_0__BOSS_2",())) )) \
 
 // If the default version has been loaded, set it as default.
