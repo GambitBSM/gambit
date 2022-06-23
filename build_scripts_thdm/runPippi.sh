d207544d95d4882be22adb4ce1eb8f25bbd77462
Tomas Gonzalo <tomas.gonzalo@kit.edu>
2022-06-23 11:03:41 +0200

    remove scripts

--- build_scripts_thdm/runPippi.sh
+++
@@ -1,7 +0,0 @@
-HEADING='=============================='
-BLUE="\n${HEADING}\n\033[1;34m[GAMBIT-BUILD]  "
-NO_COL="\033[0m\n${HEADING}\n\n"
-
-echo -e "${BLUE}running pippi${NO_COL}"
-
-../pippi/pippi plotRules.pip
