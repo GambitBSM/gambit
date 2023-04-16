08bffb2443a30fac88e7c676143cb46850129483
Tomek Procter <tprocter46@gmail.com>
2023-04-16 15:06:25 +0100

    Add BOSS generated Rivet 3.1.7 files

---
+++ Backends/include/gambit/Backends/backend_types/Rivet_3_1_7/identification.hpp
@@ -0,0 +1,12 @@
+// Identify backend and set macro flags
+
+#include "gambit/Utils/cats.hpp"
+
+#define BACKENDNAME Rivet
+#define BACKENDLANG CXX
+#define VERSION 3.1.7
+#define SAFE_VERSION 3_1_7
+#define REFERENCE Bierlich:2019rhm
+
+#undef DO_CLASSLOADING
+#define DO_CLASSLOADING 1
