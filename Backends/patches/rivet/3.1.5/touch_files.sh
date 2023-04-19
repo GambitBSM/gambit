0ac43724aa9d05ecc1fb02be620f1e6dc5182405
Tomek Procter <tprocter46@gmail.com>
2023-04-19 21:59:07 +0100

    Remove last vestiges of rivet 3.1.5

--- Backends/patches/rivet/3.1.5/touch_files.sh
+++
@@ -1,3 +0,0 @@
-for dir in $Rivet_dirs; do for file in "$dir"/*lo; do [ -e "$file" ] && touch $file; done; done
-for dir in $Rivet_dirs; do for file in "$dir"/*la; do [ -e "$file" ] && touch $file; done; done
-echo "Reseting library modification times"
