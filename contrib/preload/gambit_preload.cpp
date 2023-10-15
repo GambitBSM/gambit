04b37c4a2d49c2915360c1868029ff1052d0c286
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2023-10-16 01:31:28 +0200

    Translated gambit_preload.cpp from C style to C++ style.
    This fixes a (pedantic) compilation error about variable length arrays when RestFrames is used.

--- contrib/preload/gambit_preload.cpp
+++ contrib/preload/gambit_preload.cpp
@@ -14,36 +14,38 @@
 ///          p.scott@imperial.ac.uk
 ///  \date 2019 June, July
 ///
+///  \author Anders Kvellestad
+///          anders.kvellestad@fys.uio.no
+///  \date 2023 Oct
+///
 ///  *********************************************
 
 #include <cstdlib>
-#include <cstdio>
-#include <cstring>
+#include <string>
+#include <iostream>
 
 #include "gambit/cmake/cmake_variables.hpp"
 #include "gambit/Utils/stringify.hpp"
-
 
 // Initializer; runs as soon as this library is loaded.
 __attribute__((constructor))
 static void initializer()
 {
-  printf("%s", "\n\x1b[1;33mGAMBIT " STRINGIFY(GAMBIT_VERSION_MAJOR) "." STRINGIFY(GAMBIT_VERSION_MINOR) "." STRINGIFY(GAMBIT_VERSION_REVISION));
-  if (strcmp(GAMBIT_VERSION_PATCH, "") != 0) printf("%s", "-" GAMBIT_VERSION_PATCH);
-  printf("\nhttp://gambit.hepforge.org\n\n\x1b[0m");
+  std::cout << "\n\x1b[1;33mGAMBIT " << STRINGIFY(GAMBIT_VERSION_MAJOR) << "." << STRINGIFY(GAMBIT_VERSION_MINOR) << "." << STRINGIFY(GAMBIT_VERSION_REVISION);
+  if (std::string(GAMBIT_VERSION_PATCH) != "") std::cout << "-" << GAMBIT_VERSION_PATCH;
+  std::cout << "\nhttp://gambitbsm.org\n\n\x1b[0m";
+
   #ifndef EXCLUDE_RESTFRAMES
   {
     const char* oldenv = getenv("CPLUS_INCLUDE_PATH");
-    const char* addition = (oldenv == NULL ? RESTFRAMES_INCLUDE : ":" RESTFRAMES_INCLUDE);
+    std::string newenv;
     if (oldenv != NULL)
     {
-      char newenv[strlen(oldenv) + strlen(addition) + 1];
-      strcpy(newenv, oldenv);
-      strcat(newenv, addition);
-      setenv("CPLUS_INCLUDE_PATH", newenv, 1);
+      newenv = std::string(oldenv);
+      newenv += ":";
     }
-    else setenv("CPLUS_INCLUDE_PATH", addition, 1);
+    newenv += RESTFRAMES_INCLUDE;
+    setenv("CPLUS_INCLUDE_PATH", newenv.c_str(), 1);
   }
   #endif
-
 }
