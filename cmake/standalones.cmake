75de7ca9bb326c220b145f05cf6b7fb538b71a67
ChrisJChang <59903135+ChrisJChang@users.noreply.github.com>
2023-12-06 19:31:18 +0100

    Change standalone dependency to FlavBit since ExampleBit_A will face the same issue

--- cmake/standalones.cmake
+++ cmake/standalones.cmake
@@ -26,9 +26,9 @@
 # Add some programs that use the GAMBIT physics libraries but not GAMBIT itself.
 add_standalone(ExampleBit_A_standalone SOURCES ExampleBit_A/examples/ExampleBit_A_standalone_example.cpp MODULES ExampleBit_A)
 add_standalone(CBS SOURCES ColliderBit/examples/solo.cpp MODULES ColliderBit DEPENDENCIES hepmc pybind11)
-add_standalone(DarkBit_standalone_MSSM SOURCES DarkBit/examples/DarkBit_standalone_MSSM.cpp MODULES DarkBit DEPENDENCIES ExampleBit_A_standalone)
-add_standalone(DarkBit_standalone_ScalarSingletDM_Z2 SOURCES DarkBit/examples/DarkBit_standalone_ScalarSingletDM_Z2.cpp MODULES DarkBit DEPENDENCIES ExampleBit_A_standalone)
-add_standalone(DarkBit_standalone_WIMP SOURCES DarkBit/examples/DarkBit_standalone_WIMP.cpp MODULES DarkBit DEPENDENCIES pybind11 ExampleBit_A_standalone)
+add_standalone(DarkBit_standalone_MSSM SOURCES DarkBit/examples/DarkBit_standalone_MSSM.cpp MODULES DarkBit DEPENDENCIES FlavBit_standalone)
+add_standalone(DarkBit_standalone_ScalarSingletDM_Z2 SOURCES DarkBit/examples/DarkBit_standalone_ScalarSingletDM_Z2.cpp MODULES DarkBit DEPENDENCIES FlavBit_standalone)
+add_standalone(DarkBit_standalone_WIMP SOURCES DarkBit/examples/DarkBit_standalone_WIMP.cpp MODULES DarkBit DEPENDENCIES pybind11 FlavBit_standalone)
 add_standalone(3bithit SOURCES DecayBit/examples/3bithit.cpp MODULES DecayBit SpecBit PrecisionBit)
 add_standalone(FlavBit_standalone SOURCES FlavBit/examples/FlavBit_standalone_example.cpp MODULES FlavBit)
 
