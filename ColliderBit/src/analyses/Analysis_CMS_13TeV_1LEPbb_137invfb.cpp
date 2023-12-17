37757d14df7f10d10c3eb2dfed839fbca7524332
Buding <zhupengxuan326@hotmail.com>
2023-12-18 00:47:16 +0800

    Pengxuan: validation for CMS-PAS-SUS_23-003

--- ColliderBit/src/analyses/Analysis_CMS_13TeV_1LEPbb_137invfb.cpp
+++ ColliderBit/src/analyses/Analysis_CMS_13TeV_1LEPbb_137invfb.cpp
@@ -19,6 +19,7 @@
 #include "HEPUtils/FastJet.h"
 
 using namespace std;
+#define CHECK_CUTFLOW
 
 namespace Gambit
 {
@@ -29,8 +30,15 @@ namespace Gambit
 
     class Analysis_CMS_13TeV_1LEPbb_137invfb : public Analysis
     {
+    // protected:
+
+    //     Cutflow _cutflows;
 
     public:
+
+      #ifdef CHECK_CUTFLOW
+        Cutflows _cutflows;
+      #endif
       // Required detector sim
       static constexpr const char *detector = "CMS";
 
@@ -43,14 +51,67 @@ namespace Gambit
 
         set_analysis_name("CMS_13TeV_1LEPbb_137invfb");
         set_luminosity(137.0);
+
+        #ifdef CHECK_CUTFLOW
+          cout << "Starting the Run Analysis \n booking Cutflows" << endl; 
+          // Book Cutflows
+          const vector<string> cutnames = {
+                                  "no cut", 
+                                  "Pre-selection",
+                                  "No-veto-Lepton",
+                                  "No-veto-hadronic taus",
+                                  "1 Single Lepton",
+                                  "mT(l) >= 150 GeV",
+                                  "N_bjet = 2",
+                                  "N_jet = 2", 
+                                  "N_jet = 3",
+                                  "ETmiss >= 125 GeV",
+                                  "90 GeV <= mbb <= 150 GeV",
+                                  "mCT >= 200 GeV",
+                                  "1 AK8 Jet",
+                                  "1 Higgs Jet",
+                                  "Selection criteria",
+                                  "SR2J-0H",
+                                  "SR2J-1H",
+                                  "SR3J-0H",
+                                  "SR3J-1H"
+                                  };
+
+          // _cutflows.addCutflow("SR2J-0H-1", cutnames);
+          // _cutflows.addCutflow("SR2J-0H-2", cutnames);
+          // _cutflows.addCutflow("SR2J-0H-3", cutnames);
+          // _cutflows.addCutflow("SR2J-0H-4", cutnames); 
+
+          // _cutflows.addCutflow("SR2J-1H-1", cutnames);
+          // _cutflows.addCutflow("SR2J-1H-2", cutnames);
+          
+          // _cutflows.addCutflow("SR3J-0H-1", cutnames);
+          // _cutflows.addCutflow("SR3J-0H-2", cutnames);
+          // _cutflows.addCutflow("SR3J-0H-3", cutnames);
+          // _cutflows.addCutflow("SR3J-0H-4", cutnames);      
+              
+          // _cutflows.addCutflow("SR3J-1H-1", cutnames);
+          // _cutflows.addCutflow("SR3J-1H-2", cutnames);
+
+          _cutflows.addCutflow("CMS_13TeV_1LEPbb_137invfb", cutnames);
+          
+          // cout << _cutflows << endl; 
+        #endif  
       }
 
       void run(const HEPUtils::Event *event)
       {
+        #ifdef CHECK_CUTFLOW
+          const double w = event->weight();
+          _cutflows["CMS_13TeV_1LEPbb_137invfb"].fillinit(w);
+          _cutflows["CMS_13TeV_1LEPbb_137invfb"].fillnext(w); // no cut
+        #endif
         ////////////////////////
         // Useful definiitons //
         // Baseline objects 
         double met = event->met();
+        // double w = event->weight();
+        // cout << "Event weight is -> " << w << endl; 
 
         // Baseline ELectrons
         BASELINE_PARTICLES(event->electrons(), baselineElectrons, 30, 0, DBL_MAX, 1.44, CMS::eff2DEl.at("SUS_19_008"))       
@@ -175,35 +236,29 @@ namespace Gambit
         SIGNAL_JETS(baselineBJets, signalBJets);
         SIGNAL_JETS(baselineLargeRJets, signalJets_AK8)
 
-
-        // std::sort(signalJets.begin(), signalJets.end(), sortByPT14);
-        // std::sort(signalBJets.begin(), signalBJets.end(), sortByPT14);
-        // std::sort(signalNonBJets.begin(), signalNonBJets.end(), sortByPT14);
-
-        // vector<std::shared_ptr<HEPUtils::Jet>>LargeRJets = get_jets(signalJets, 0.8);
-        // std::sort(LargeRJets.begin(), LargeRJets.end(), sortByPT14_sharedptr);
-
-        // vector<std::shared_ptr<HEPUtils::Jet>> signalLargeRJets;
-        // for (const auto& jet : LargeRJets)
-        // {
-        //   if (jet->pT() > 250. && jet->abseta() < 2.4)
-        //   {
-        //     signalLargeRJets.push_back(jet);
-        //   }
-        // }
-
         removeOverlap(signalJets_AK8, signalLeptons, 0.8);
         removeOverlap(signalJets_AK4, signalLeptons, 0.4);
         removeOverlap(signalBJets, signalLeptons, 0.4);
 
-        bool lepton2_veto = true;
-        if (vetoLeptons.size() > 1)
-          lepton2_veto = false;
 
-        bool tau_veto = true;
-        if (baselineTaus.size() > 0)
-          tau_veto = false;
+        #ifdef CHECK_CUTFLOW
+          _cutflows["CMS_13TeV_1LEPbb_137invfb"].fill(2, w); // no cut
+        #endif
+        
+        if (vetoLeptons.size() > 1) return; 
 
+        #ifdef CHECK_CUTFLOW
+          _cutflows["CMS_13TeV_1LEPbb_137invfb"].fill(3, w); // no cut
+        #endif
+
+        if (baselineTaus.size() > 0) return; 
+        
+        #ifdef CHECK_CUTFLOW
+          _cutflows["CMS_13TeV_1LEPbb_137invfb"].fill(4, w); // no cut
+        #endif
+        // #ifdef CHECK_CUTFLOW
+        //   _cutflows.fill(4, event->weight()); // no cut
+        // #endif
         // flag of preselection requirement; Table 3
         bool nlepton_ps = false;
         bool nsRjs_ps = false;
@@ -213,96 +268,144 @@ namespace Gambit
         bool mct_ps = false;
 
         double mbb;
-        double mCT;
+        double mct;
         double mTl;
         int NHjet = 0;
         int Njets = signalJets_AK4.size();
 
-        if (signalLeptons.size() == 1 && lepton2_veto && tau_veto)
+        if (signalLeptons.size() == 1)
         {
           nlepton_ps = true;
+          #ifdef CHECK_CUTFLOW
+            _cutflows["CMS_13TeV_1LEPbb_137invfb"].fill(5, w); // no cut
+          #endif
+
           mTl = mT(signalLeptons.at(0)->mom(), event->missingmom());
+
           if (mTl > 150.)
+          {
             mt_ps = true;
+            #ifdef CHECK_CUTFLOW
+              _cutflows["CMS_13TeV_1LEPbb_137invfb"].fill(6, w); // no cut
+            #endif
+          }
         }
         if (Njets == 2 && signalBJets.size() == 2)
+        {
           nsRjs_ps = true;
+          #ifdef CHECK_CUTFLOW
+            _cutflows["CMS_13TeV_1LEPbb_137invfb"].fill(7, w); // no cut
+            _cutflows["CMS_13TeV_1LEPbb_137invfb"].fill(8, w); // no cut
+          #endif
+        }
+
         if (Njets == 3 && signalBJets.size() == 2)
         {
-          if (signalJets_AK4[0]->pT() < 300.)
+          if (signalJets_AK4[0]->pT() < 300.){
             nsRjs_ps = true;
+            #ifdef CHECK_CUTFLOW
+              _cutflows["CMS_13TeV_1LEPbb_137invfb"].fill(7, w); // no cut
+              _cutflows["CMS_13TeV_1LEPbb_137invfb"].fill(9, w); // no cut
+            #endif            
+          }
         }
-        if (met > 125.)
+        if (met > 125.){
           ptmiss_ps = true;
+          #ifdef CHECK_CUTFLOW
+            _cutflows["CMS_13TeV_1LEPbb_137invfb"].fill(10, w); // no cut
+          #endif  
+        }
+
         if (nsRjs_ps)
         {
           mbb = (signalBJets.at(0)->mom() + signalBJets.at(1)->mom()).m();
-          mCT = mT(signalBJets.at(0)->mom(), signalBJets.at(1)->mom());
+          mct = mCT(signalBJets.at(0)->mom(), signalBJets.at(1)->mom());
           if (mbb > 90. && mbb < 150.)
-            mbb_ps = true;
-          if (mCT > 200.)
+          {
+            mbb_ps = true;          
+            #ifdef CHECK_CUTFLOW
+              _cutflows["CMS_13TeV_1LEPbb_137invfb"].fill(11, w); 
+            #endif  
+          }
+          if (mct > 200.)
+          {
             mct_ps = true;
+            #ifdef CHECK_CUTFLOW
+              _cutflows["CMS_13TeV_1LEPbb_137invfb"].fill(12, w); 
+            #endif  
+          }
           if (signalJets_AK8.size() == 1)
           {
+            #ifdef CHECK_CUTFLOW
+              _cutflows["CMS_13TeV_1LEPbb_137invfb"].fill(13, w); 
+            #endif 
             if ((signalJets_AK8.at(0)->mom().deltaR_eta(signalBJets.at(0)->mom()) < 0.8) && (signalJets_AK8.at(0)->mom().deltaR_eta(signalBJets.at(1)->mom()) < 0.8))
+            {  
               NHjet = 1;
+              #ifdef CHECK_CUTFLOW
+                _cutflows["CMS_13TeV_1LEPbb_137invfb"].fill(14, w); 
+              #endif 
+            }
           }
         }
+
 
         // Counting numbers of SRs
+        // if (nlepton_ps && nsRjs_ps && ptmiss_ps && mbb_ps && mt_ps && mct_ps)
         if (nlepton_ps && nsRjs_ps && ptmiss_ps && mbb_ps && mt_ps && mct_ps)
         {
-          if (NHjet == 0 && Njets == 2 && met >= 125. && met < 200.)
+          #ifdef CHECK_CUTFLOW
+            _cutflows["CMS_13TeV_1LEPbb_137invfb"].fill(15, w); 
+          #endif 
+
+          if (NHjet == 0 && Njets == 2)
           {
-            FILL_SIGNAL_REGION("SR2J-0H-1");
-          }
-          if (NHjet == 0 && Njets == 2 && met >= 200. && met < 300.)
-          {
-            FILL_SIGNAL_REGION("SR2J-0H-2");
-          }
-          if (NHjet == 0 && Njets == 2 && met >= 300. && met < 400.)
-          {
-            FILL_SIGNAL_REGION("SR2J-0H-3");
-          }
-          if (NHjet == 0 && Njets == 2 && met >= 400.)
-          {
-            FILL_SIGNAL_REGION("SR2J-0H-4");
-          }
-          if (NHjet == 1 && Njets == 2 && met >= 125. && met < 300.)
-          {
-            FILL_SIGNAL_REGION("SR2J-1H-1");
-          }
-          if (NHjet == 1 && Njets == 2 && met >= 300.)
-          {
-            FILL_SIGNAL_REGION("SR2J-1H-2");
+            if (met >= 125. && met < 200.) {_counters.at("SR2J-0H-1").add_event(event);}
+            if (met >= 200. && met < 300.) {_counters.at("SR2J-0H-2").add_event(event);}
+            if (met >= 300. && met < 400.) {_counters.at("SR2J-0H-3").add_event(event);}
+            if (met >= 400.)               {_counters.at("SR2J-0H-4").add_event(event);}
+
+            #ifdef CHECK_CUTFLOW
+              _cutflows["CMS_13TeV_1LEPbb_137invfb"].fill(16, w); 
+            #endif     
+
           }
 
-          if (NHjet == 0 && Njets == 3 && met >= 125. && met < 200.)
+          if (NHjet == 1 && Njets == 2)
           {
-            FILL_SIGNAL_REGION("SR3J-0H-1");
+            if (met >= 125. && met < 300.) {_counters.at("SR2J-1H-1").add_event(event);}
+            if (met >= 300.)               {_counters.at("SR2J-1H-2").add_event(event);}
+
+            #ifdef CHECK_CUTFLOW
+              _cutflows["CMS_13TeV_1LEPbb_137invfb"].fill(17, w); 
+            #endif     
+
           }
-          if (NHjet == 0 && Njets == 3 && met >= 200. && met < 300.)
+
+          if (NHjet == 0 && Njets == 3)
           {
-            FILL_SIGNAL_REGION("SR3J-0H-2");
+            if (met >= 125. && met < 200.) {_counters.at("SR3J-0H-1").add_event(event);}
+            if (met >= 200. && met < 300.) {_counters.at("SR3J-0H-2").add_event(event);}
+            if (met >= 300. && met < 400.) {_counters.at("SR3J-0H-3").add_event(event);}
+            if (met >= 400.)               {_counters.at("SR3J-0H-4").add_event(event);}
+
+            #ifdef CHECK_CUTFLOW
+              _cutflows["CMS_13TeV_1LEPbb_137invfb"].fill(18, w); 
+            #endif     
           }
-          if (NHjet == 0 && Njets == 3 && met >= 300. && met < 400.)
+
+          if (NHjet == 1 && Njets == 3)
           {
-            FILL_SIGNAL_REGION("SR3J-0H-3");
+            if (met >= 125. && met < 300.) {_counters.at("SR3J-1H-1").add_event(event);}
+            if (met >= 300.)               {_counters.at("SR3J-1H-2").add_event(event);}
+
+            #ifdef CHECK_CUTFLOW
+              _cutflows["CMS_13TeV_1LEPbb_137invfb"].fill(19, w); 
+            #endif     
           }
-          if (NHjet == 0 && Njets == 3 && met >= 400.)
-          {
-            FILL_SIGNAL_REGION("SR3J-0H-4");
-          }
-          if (NHjet == 1 && Njets == 3 && met >= 125. && met < 300.)
-          {
-            FILL_SIGNAL_REGION("SR3J-1H-1");
-          }
-          if (NHjet == 1 && Njets == 3 && met >= 300.)
-          {
-            FILL_SIGNAL_REGION("SR3J-1H-2");
-          }
-        }
-        // if
+        }      
+        
+        return; 
       }
 
       virtual void collect_results()
@@ -311,24 +414,27 @@ namespace Gambit
         // Commit the results for signal regions, included observed and bacground counts
         // COMMIT_SIGNAL_REGION(SR, OBS, BKG, BKG_ERR)
 
-        COMMIT_SIGNAL_REGION("SR2J-0H-1", 8., 6.9, 1.3);
-        COMMIT_SIGNAL_REGION("SR2J-0H-2", 2., 3.4, 0.6);
-        COMMIT_SIGNAL_REGION("SR2J-0H-3", 1., 1.0, 0.3);
-        COMMIT_SIGNAL_REGION("SR2J-0H-4", 1., 0.3, 0.1);
-
-        COMMIT_SIGNAL_REGION("SR2J-1H-1", 3., 2.5, 0.9);
-        COMMIT_SIGNAL_REGION("SR2J-1H-2", 1., 0.9, 0.5);
-
-        COMMIT_SIGNAL_REGION("SR3J-0H-1", 17., 17.8, 3.0);
-        COMMIT_SIGNAL_REGION("SR3J-0H-2", 6., 7.8, 1.7);
-        COMMIT_SIGNAL_REGION("SR3J-0H-3", 0., 1.5, 0.5);
-        COMMIT_SIGNAL_REGION("SR3J-0H-4", 0., 0.5, 0.3);
-
-        COMMIT_SIGNAL_REGION("SR3J-1H-1", 10., 5.9, 2.1);
-        COMMIT_SIGNAL_REGION("SR3J-1H-2", 0., 2.1, 0.6);
+        add_result(SignalRegionData(_counters.at("SR2J-0H-1"), 8.,  {6.9,  1.3}));
+        add_result(SignalRegionData(_counters.at("SR2J-0H-2"), 2.,  {3.4,  0.6}));
+        add_result(SignalRegionData(_counters.at("SR2J-0H-3"), 1.,  {1.0,  0.3}));
+        add_result(SignalRegionData(_counters.at("SR2J-0H-4"), 1.,  {0.3,  0.1}));
+        add_result(SignalRegionData(_counters.at("SR2J-1H-1"), 3.,  {2.5,  0.9}));
+        add_result(SignalRegionData(_counters.at("SR2J-1H-2"), 1.,  {0.9,  0.5}));
+        add_result(SignalRegionData(_counters.at("SR3J-0H-1"), 17., {17.8, 3.0}));
+        add_result(SignalRegionData(_counters.at("SR3J-0H-2"), 6.,  {7.8,  1.7}));
+        add_result(SignalRegionData(_counters.at("SR3J-0H-3"), 0.,  {1.5,  0.5}));
+        add_result(SignalRegionData(_counters.at("SR3J-0H-4"), 0.,  {0.5,  0.3}));
+        add_result(SignalRegionData(_counters.at("SR3J-1H-1"), 10., {5.9,  2.1}));
+        add_result(SignalRegionData(_counters.at("SR3J-1H-2"), 0.,  {2.1,  0.6}));
 
         // Add cutflow data to the analysis results
         COMMIT_CUTFLOWS;
+          // _cutflows.combine();
+        cout << "\nCUTFLOWS:\n" << _cutflows << endl;
+        cout << "\nSRCOUNTS:\n";
+        // Note: The sum() call below gives the raw event count. Use weight_sum() for the sum of event weights.
+        for (auto& pair : _counters) cout << pair.first << " ->\t"<< pair.second.sum() << "\n";
+        cout << "\n" << endl;
       }
 
       double mT(const HEPUtils::P4 &pV, const HEPUtils::P4 &pI)
@@ -338,6 +444,15 @@ namespace Gambit
             2 * pV.pT() * pI.pT() * (1 - cos(deltaPhi)));
 
         return mT;
+      }
+      
+      double mCT(const HEPUtils::P4 &pV, const HEPUtils::P4 &pI)
+      {
+        double deltaPhi = pV.deltaPhi(pI);
+        double mCT = sqrt(
+            2 * pV.pT() * pI.pT() * (1 + cos(deltaPhi)));
+
+        return mCT;
       }
 
     protected:
