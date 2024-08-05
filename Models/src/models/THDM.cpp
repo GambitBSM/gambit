be20b604b5f3c49fd638148400c88150bf9772ad
A.S.Woodcock <alex.woodcock@outlook.com>
2024-08-05 10:37:36 +0100

    fix mh2, mH2, etc bug

--- Models/src/models/THDM.cpp
+++ Models/src/models/THDM.cpp
@@ -82,7 +82,34 @@ void MODEL_NAMESPACE::THDM_physical_to_THDM (const ModelParameters &myP, ModelPa
 {
   USE_MODEL_PIPE(PARENT) // get pipe for "interpret as PARENT" function
   logger()<<"Running interpret_as_parent calculations for THDM_physical --> THDM.."<<LogTags::info<<EOM;
-  physical_to_generic(*Dep::SMINPUTS, myP, targetP);
+
+  const SMInputs& sminputs = *Dep::SMINPUTS;
+
+  //generic basis
+  double mh2 =  myP.getValue("mh2"), mH2 =  myP.getValue("mH2"), mA2 =  myP.getValue("mA2"), mC2 =  myP.getValue("mC2");
+  double tb  = myP.getValue("tanb");
+  double beta = atan(tb);
+  double alpha = beta - asin(myP.getValue("sba"));
+  double ca = cos(alpha), sa = sin(alpha);
+  double v2 = 1.0/(sqrt(2.0)*sminputs.GF);
+  double ctb  = 1./tb;
+  double cb = cos(beta), sb = sin(beta);
+  double lambda1 = (mH2*pow(ca,2)+mh2*pow(sa,2)-myP.getValue("m12_2")*tb)/v2/pow(cb,2)-1.5*myP.getValue("lambda6")*tb+0.5*myP.getValue("lambda7")*pow(tb,3);
+  double lambda2 = (mH2*pow(sa,2)+mh2*pow(ca,2)-myP.getValue("m12_2")*ctb)/v2/pow(sb,2)+0.5*myP.getValue("lambda6")*pow(ctb,3)-1.5*myP.getValue("lambda7")*ctb;
+  double lambda3 = ((mH2-mh2)*ca*sa+2.*mC2*sb*cb-myP.getValue("m12_2"))/v2/sb/cb-0.5*myP.getValue("lambda6")*ctb-0.5*myP.getValue("lambda7")*tb;
+  double lambda4 = ((mA2-2.*mC2)*cb*sb+myP.getValue("m12_2"))/v2/sb/cb-0.5*myP.getValue("lambda6")*ctb-0.5*myP.getValue("lambda7")*tb;
+  double lambda5 = (myP.getValue("m12_2")-mA2*sb*cb)/v2/sb/cb-0.5*myP.getValue("lambda6")*ctb-0.5*myP.getValue("lambda7")*tb;
+
+  targetP.setValue("lambda1",lambda1);
+  targetP.setValue("lambda2",lambda2);
+  targetP.setValue("lambda3",lambda3);
+  targetP.setValue("lambda4",lambda4);
+  targetP.setValue("lambda5",lambda5);
+  targetP.setValue("lambda6",myP.getValue("lambda6"));
+  targetP.setValue("lambda7",myP.getValue("lambda7"));
+  targetP.setValue("m12_2", myP.getValue("m12_2"));
+  targetP.setValue("tanb", myP.getValue("tanb"));
+  
   add_Yukawas(0, *Dep::SMINPUTS, myP, targetP);
 }
 #undef PARENT
