80134ac63712bc5cc6ce522746a9f060500542c9
Pengxuan Zhu <zhupx99@proton.me>
2025-01-15 11:08:51 +1030

    Adding ATLAS-SUSY-2018-007

--- ColliderBit/src/analyses/Analysis_ATLAS_13TeV_1LEPStop_139invfb.cpp
+++ ColliderBit/src/analyses/Analysis_ATLAS_13TeV_1LEPStop_139invfb.cpp
@@ -6,18 +6,17 @@
 #include "gambit/ColliderBit/mt2_bisect.h"
 #include "METSignificance/METSignificance.hpp"
 
-
 #include <vector>
 #include <cmath>
 #include <memory>
 #include <iomanip>
 #include <algorithm>
 #include <fstream>
-// #define CHECK_CUTFLOW 
+// #define CHECK_CUTFLOW
 
-/*  The ATLAS 1 Lepton direct stop analysis 
+/*  The ATLAS 1 Lepton direct stop analysis
 
-    Based on: 
+    Based on:
         - https://atlas.web.cern.ch/Atlas/GROUPS/PHYSICS/PAPERS/SUSY-2018-07/#
         - arXiv:2012.03799
         - https://inspirehep.net/literature/1835604
@@ -26,50 +25,52 @@
 
 */
 
-
+using namespace std;
 
 namespace Gambit
 {
   namespace ColliderBit
   {
-    using namespace std;
-
 
     /// Basic analysis code for copying
     class Analysis_ATLAS_13TeV_1LEPStop_139invfb : public Analysis
     {
     private:
-
       YODA::Histo1DPtr _hist_Topness;
 
     public:
-
       // Required detector sim
-      static constexpr const char* detector = "ATLAS";
+      static constexpr const char *detector = "ATLAS";
 
       Analysis_ATLAS_13TeV_1LEPStop_139invfb()
       {
         // Signal region counter
-        _counters["SR-tN_med"]          = EventCounter("SR-tN_med");
-        _counters["SR-tN_high"]         = EventCounter("SR-tN_high"); 
-        _counters["SR-tN_diag_low"]     = EventCounter("SR-tN_diag_low");
-        _counters["SR-tN_diag_high"]    = EventCounter("SR-tN_diag_high"); 
-        _counters["SR-bWN"]             = EventCounter("SR-bWN"); 
-        _counters["SR-bffN_btag"]       = EventCounter("SR-bffN_btag"); 
-        _counters["SR-bffN_softb"]      = EventCounter("SR-bffN_softb"); 
-        _counters["SR-DM"]              = EventCounter("SR-DM"); 
+        DEFINE_SIGNAL_REGION("SR-tN_med");
+        DEFINE_SIGNAL_REGION("SR-tN_high");
+        DEFINE_SIGNAL_REGION("SR-tN_diag_low");
+        DEFINE_SIGNAL_REGION("SR-tN_diag_high");
+        DEFINE_SIGNAL_REGION("SR-bWN");
+        DEFINE_SIGNAL_REGION("SR-bffN_btag");
+        DEFINE_SIGNAL_REGION("SR-bffN_softb");
+        DEFINE_SIGNAL_REGION("SR-DM");
+        
+        // _counters["SR-tN_med"] = EventCounter("SR-tN_med");
+        // _counters["SR-tN_high"] = EventCounter("SR-tN_high");
+        // _counters["SR-tN_diag_low"] = EventCounter("SR-tN_diag_low");
+        // _counters["SR-tN_diag_high"] = EventCounter("SR-tN_diag_high");
+        // _counters["SR-bWN"] = EventCounter("SR-bWN");
+        // _counters["SR-bffN_btag"] = EventCounter("SR-bffN_btag");
+        // _counters["SR-bffN_softb"] = EventCounter("SR-bffN_softb");
+        // _counters["SR-DM"] = EventCounter("SR-DM");
 
         // Set the analysis name
         set_analysis_name("ATLAS_13TeV_1LEPStop_139invfb");
 
         // Set the LHC luminosity
         set_luminosity(139.0);
-
-
       }
 
-
-      void run(const HEPUtils::Event* event)
+      void run(const HEPUtils::Event *event)
       {
 
         // Get the missing energy in the event
@@ -80,45 +81,52 @@ namespace Gambit
         // - application of basic pT and eta cuts
 
         // Baseline electrons
-        vector<const HEPUtils::Particle*> baselineElectrons;
-        for (const HEPUtils::Particle* electron : event->electrons()) {
-          if (electron->pT() > 4.5 && fabs(electron->eta()) < 2.47) baselineElectrons.push_back(electron);
+        vector<const HEPUtils::Particle *> baselineElectrons;
+        for (const HEPUtils::Particle *electron : event->electrons())
+        {
+          if (electron->pT() > 4.5 && fabs(electron->eta()) < 2.47)
+            baselineElectrons.push_back(electron);
         }
 
         // Baseline muons
-        vector<const HEPUtils::Particle*> baselineMuons;
-        for (const HEPUtils::Particle* muon : event->muons()) {
-          if (muon->pT() > 4.0 && fabs(muon->eta()) < 2.7) baselineMuons.push_back(muon);
+        vector<const HEPUtils::Particle *> baselineMuons;
+        for (const HEPUtils::Particle *muon : event->muons())
+        {
+          if (muon->pT() > 4.0 && fabs(muon->eta()) < 2.7)
+            baselineMuons.push_back(muon);
         }
 
         // Baseline jets
-        vector<const HEPUtils::Jet*> baselineJets;
-        for (const HEPUtils::Jet* jet : event->jets("antikt_R04")) {
-          if (jet->pT() > 20. && fabs(jet->eta()) < 4.5) baselineJets.push_back(jet);
-        }
-
-        vector<const HEPUtils::Jet *> bJets; 
-        vector<const HEPUtils::Jet *> nonbJets;  
-
-        // B-tag Efficiencies 
-        std::map<const Jet*, bool> analysisBtags = generateBTagsMap(baselineJets, 0.77, 1.0/6.0, 1.0/130.); 
-        for (const HEPUtils::Jet* jet : baselineJets)
+        vector<const HEPUtils::Jet *> baselineJets;
+        for (const HEPUtils::Jet *jet : event->jets("antikt_R04"))
         {
-          bool isBTag = analysisBtags.at(jet); 
-          if (isBTag && jet->pT() > 20. ) bJets.push_back(jet);
-          else nonbJets.push_back(jet);  
+          if (jet->pT() > 20. && fabs(jet->eta()) < 4.5)
+            baselineJets.push_back(jet);
         }
 
-        vector<const HEPUtils::Particle *> signalTaus; 
-        for (const HEPUtils::Particle * tau : event->taus())
+        vector<const HEPUtils::Jet *> bJets;
+        vector<const HEPUtils::Jet *> nonbJets;
+
+        // B-tag Efficiencies
+        std::map<const Jet *, bool> analysisBtags = generateBTagsMap(baselineJets, 0.77, 1.0 / 6.0, 1.0 / 130.);
+        for (const HEPUtils::Jet *jet : baselineJets)
         {
-          if (tau->pT() > 20. && fabs(tau->eta()) < 2.5) signalTaus.push_back(tau); 
+          bool isBTag = analysisBtags.at(jet);
+          if (isBTag && jet->pT() > 20.)
+            bJets.push_back(jet);
+          else
+            nonbJets.push_back(jet);
         }
-        applyEfficiency(signalTaus, ATLAS::effTau.at("R2_RNN_Loose")); 
 
-        removeOverlap(baselineJets, baselineElectrons, 0.2, true); 
-        
+        vector<const HEPUtils::Particle *> signalTaus;
+        for (const HEPUtils::Particle *tau : event->taus())
+        {
+          if (tau->pT() > 20. && fabs(tau->eta()) < 2.5)
+            signalTaus.push_back(tau);
+        }
+        applyEfficiency(signalTaus, ATLAS::effTau.at("R2_RNN_Loose"));
 
+        removeOverlap(baselineJets, baselineElectrons, 0.2, true);
 
         // Could add ATLAS style overlap removal here
         // See Analysis_ATLAS_0LEP_20invfb for example
@@ -137,38 +145,37 @@ namespace Gambit
 
         // if((nElectrons+nMuons)==0 && nJets==2 && met>150.) _counters["SR"].add_event(event);
 
-        return; 
-
+        return;
       }
 
-
-      void collect_results() {
+      void collect_results()
+      {
 
         // Now fill a results object with the result for our signal region
         // We have made up a number of observed events
         // We have also made up a number of predicted background events (with a made up uncertainty)
 
         // add_result(SignalRegionData(_counters["SR label"], n_obs, {n_bkg, n_bkg_err}));
-        add_result(SignalRegionData(_counters["SR-tN_med"],         21., {21.,  4.0}));
-        add_result(SignalRegionData(_counters["SR-tN_high"],        17., {9.5,  1.6}));
-        add_result(SignalRegionData(_counters["SR-tN_diag_low"],    21., {15.,  4.0}));
-        add_result(SignalRegionData(_counters["SR-tN_diag_high"],   11., {10.1, 3.4}));
-        add_result(SignalRegionData(_counters["SR-bWN"],            35., {44.,  9.0}));
-        add_result(SignalRegionData(_counters["SR-bffN_btag"],      14., {11.3, 1.4}));
-        add_result(SignalRegionData(_counters["SR-bffN_softb"],     10., {8.7,  2.3}));
-        add_result(SignalRegionData(_counters["SR-DM"],             56., {56.0, 8.0}));
-
+        add_result(SignalRegionData(_counters["SR-tN_med"], 21., {21., 4.0}));
+        add_result(SignalRegionData(_counters["SR-tN_high"], 17., {9.5, 1.6}));
+        add_result(SignalRegionData(_counters["SR-tN_diag_low"], 21., {15., 4.0}));
+        add_result(SignalRegionData(_counters["SR-tN_diag_high"], 11., {10.1, 3.4}));
+        add_result(SignalRegionData(_counters["SR-bWN"], 35., {44., 9.0}));
+        add_result(SignalRegionData(_counters["SR-bffN_btag"], 14., {11.3, 1.4}));
+        add_result(SignalRegionData(_counters["SR-bffN_softb"], 10., {8.7, 2.3}));
+        add_result(SignalRegionData(_counters["SR-DM"], 56., {56.0, 8.0}));
       }
-
 
     protected:
       void analysis_specific_reset()
       {
-        for (auto& pair : _counters) { pair.second.reset(); }
+        for (auto &pair : _counters)
+        {
+          pair.second.reset();
+        }
       }
 
       ///////////////////
-
     };
 
     DEFINE_ANALYSIS_FACTORY(ATLAS_13TeV_1LEPStop_139invfb)
