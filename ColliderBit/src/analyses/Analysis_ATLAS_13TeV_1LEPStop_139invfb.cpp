0fb1049e91eaf3a2f4f668fb1af08be42cb1403f
Pengxuan Zhu <zhupx99@proton.me>
2025-01-20 18:51:18 +1030

    Adding ATLAS-SUSY-2018-007: Soft b jet object defined

--- ColliderBit/src/analyses/Analysis_ATLAS_13TeV_1LEPStop_139invfb.cpp
+++ ColliderBit/src/analyses/Analysis_ATLAS_13TeV_1LEPStop_139invfb.cpp
@@ -42,15 +42,6 @@ namespace Gambit
     static bool sortByPT_jet(const HEPUtils::Jet *jet1, const HEPUtils::Jet *jet2) { return (jet1->pT() > jet2->pT()); }
     static bool sortByPT_lep(const HEPUtils::Particle *lep1, const HEPUtils::Particle *lep2) { return (lep1->pT() > lep2->pT()); }
 
-    static double calcMT(HEPUtils::P4 lepmom, HEPUtils::P4 metMom)
-    {
-      // std::cout << "metMom.px() " << metMom.px() << " jetMom PT " << jetMom.pT() << std::endl;
-      double met = sqrt(metMom.px() * metMom.px() + metMom.py() * metMom.py());
-      double dphi = metMom.deltaPhi(lepmom);
-      double mt = sqrt(2 * lepmom.pT() * met * (1 - std::cos(dphi)));
-      return mt;
-    }
-
     /// Basic analysis code for copying
     class Analysis_ATLAS_13TeV_1LEPStop_139invfb : public Analysis
     {
@@ -81,6 +72,138 @@ namespace Gambit
         // Set the LHC luminosity
         set_luminosity(139.0);
       }
+
+      std::pair<bool, FJNS::PseudoJet> RecursiveRecluster(const FJNS::PseudoJet& candidate, double candRadius,
+                   const double mass, size_t step)
+      {
+        if (minRadius(candidate.pt(), mass) > candRadius)
+        {
+          GetHistory(candidate).steps.back().status = ClusteringHistory::JET_TOO_SMALL;
+          return std::make_pair(false, candidate);
+        }
+        else if (maxRadius(candidate.pt(), mass) < candRadius)
+        {
+          const double newR = std::max(maxRadius(candidate.pt(), mass), candRadius / 2.);
+          GetHistory(candidate).steps.back().status = ClusteringHistory::JET_TOO_LARGE;
+
+          if (step > 10)
+          {
+            GetHistory(candidate).steps.back().status = ClusteringHistory::TOO_MANY_ITERATIONS;
+            return std::make_pair(false, candidate);
+          }
+
+          FJNS::JetDefinition jetDef(FJNS::antikt_algorithm, newR);
+          auto cs = new FJNS::ClusterSequence(candidate.constituents(), jetDef);
+
+          std::vector<FJNS::PseudoJet> reclusteredJets;
+          reclusteredJets = SortedByNConstit(cs->inclusive_jets());
+
+          if (reclusteredJets.size() == 0)
+          {
+            delete cs;
+            return std::make_pair(false, FJNS::PseudoJet());
+          }
+
+          cs->delete_self_when_unused();
+          auto newCandidate = reclusteredJets[0];
+
+          auto newHistory = ClusteringHistory::AddStep(
+                   GetHistory(candidate),
+                   {newCandidate.pt(), newR, newCandidate.constituents().size(), ClusteringHistory::NONE});
+          newCandidate.set_user_info(newHistory);
+
+          return RecursiveRecluster(newCandidate, newR, mass, step + 1);
+        }
+        else
+        {
+          GetHistory(candidate).steps.back().status = ClusteringHistory::GOOD;
+          return std::make_pair(true, candidate);
+        }
+      }
+
+
+      HEPUtils::P4 reclusteredParticle(vector<const HEPUtils::Jet*> jets, vector<const HEPUtils::Jet*> bjets,
+                                       const double mass, const bool useBJets)
+      {
+
+        //AnalysisObject p = AnalysisObject(0., 0., 0., 0., 0, 0, AnalysisObjectType::JET, 0, 0);
+        HEPUtils::P4 p;
+        double r0 = 3.0;
+
+        vector<const HEPUtils::Jet*> usejets;
+        for(const HEPUtils::Jet* jet : jets)
+        {
+          usejets.push_back(jet);
+        }
+
+        if (useBJets && bjets.size())
+        {
+          for(const HEPUtils::Jet* bjet : bjets)
+          {
+            usejets.push_back(bjet);
+          }
+        }
+
+        std::vector<FJNS::PseudoJet> initialJets;
+
+        for (const HEPUtils::Jet* jet : usejets)
+        {
+          FJNS::PseudoJet Pjet(jet->mom().px(), jet->mom().py(), jet->mom().pz(), jet->mom().E());
+          initialJets.push_back(Pjet);
+        }
+
+        FJNS::JetDefinition jetDef(FJNS::antikt_algorithm, r0);
+        FJNS::ClusterSequence cs(initialJets, jetDef);
+
+        auto candidates = FJNS::sorted_by_pt(cs.inclusive_jets());
+
+        std::vector<FJNS::PseudoJet> selectedJets;
+        selectedJets.reserve(candidates.size());
+        std::vector<FJNS::PseudoJet> badJets;
+        badJets.reserve(candidates.size());
+
+        size_t i = 0;
+        for (auto& cand : candidates)
+        {
+          auto history = new ClusteringHistory();
+          history->id = i;
+          history->steps.push_back({cand.pt(), r0, cand.constituents().size(), ClusteringHistory::NONE});
+          cand.set_user_info(history);
+          ++i;
+        }
+
+        for (const auto& cand : candidates)
+        {
+          bool selected = false;
+          FJNS::PseudoJet jet;
+
+          std::tie(selected, jet) = RecursiveRecluster(cand, r0, mass, 0);
+
+          if (selected)
+            selectedJets.push_back(jet);
+          else
+            badJets.push_back(jet);
+        }
+
+        if (selectedJets.size() < 1)
+        {
+          return p;
+        }
+
+        vector<std::shared_ptr<HEPUtils::Jet>> aoSelectedJets;
+        for (const FJNS::PseudoJet& j : selectedJets) aoSelectedJets.push_back(std::make_shared<HEPUtils::Jet>(HEPUtils::mk_p4(j)));
+
+        //for (const auto jet : selectedJets)
+        //  aoSelectedJets.push_back(
+        //     AnalysisObject(jet.px(), jet.py(), jet.pz(), jet.E(), 0, 0, AnalysisObjectType::COMBINED, 0, 0));
+
+        std::sort(aoSelectedJets.begin(), aoSelectedJets.end(), sortByPT_1l_sharedptr);
+        p = aoSelectedJets[0]->mom();
+
+        return p;
+      }
+
+
 
       void run(const HEPUtils::Event *event)
       {
@@ -261,13 +384,31 @@ namespace Gambit
         const int nlephard = signalLeptons4Hard.size();
         const int njet = signalJets.size();
         const int nbjet = bJets.size();
-        const double l1PT = nlephard > 0 ? signalLeptons4Hard[0]->pT() : 0.; 
+        const double l1PT_hard = nlephard > 0 ? signalLeptons4Hard[0]->pT() : 0.;
         const double dPhijet1ET = njet > 0 ? signalJets[0]->mom().deltaPhi(metVec) : 0.;
         const double dPhijet2ET = njet > 1 ? signalJets[1]->mom().deltaPhi(metVec) : 0.;
         const double mT = nlephard > 0 ? get_mT(signalElectron4Hard[0], metVec) : 0.;
         const double mT2tau = (signalTaus.size() > 0 && nlephard > 0) ? get_mT2(signalTaus[0]->mom(), signalLeptons4Hard[0]->mom(), metVec, 0.) : 100.;
 
-        bool pre_hard = nlephard == 1 && njet >= 4 && l1PT > 25. && met > 230. && dPhijet1ET > 0.4 && dPhijet2ET > 0.4 && nbjet >= 1 && mT > 30. && mT2tau > 80. ; 
+        bool pre_hard = nlephard == 1 && njet >= 4 && l1PT_hard > 25. && met > 230. && dPhijet1ET > 0.4 && dPhijet2ET > 0.4 && nbjet >= 1 && mT > 30. && mT2tau > 80.;
+
+        const int nlepsoft = signalLeptons4Soft.size();
+        const bool softJet1 = njet >= 1 ? signalJets[0]->pT() > 200. : false;
+        const bool softJet2 = njet >= 2 ? signalJets[1]->pT() > 20. : false;
+        const double dPhijet2ET_soft = njet >= 2 ? signalJets[1]->mom().deltaPhi(metVec) : 0.5;
+
+        // All soft lepton pass the pT requirement
+        bool pre_soft = nlepsoft == 1 && (softJet1 || softJet2) && met > 230. && dPhijet1ET > 0.4 && dPhijet2ET_soft > 0.4;
+
+
+
+
+        // Define signal region for stop -> t N1
+        if (pre_hard){
+          HEPUtils::P4 topRecl = reclusteredParticle(nonbJets, bJets, 175., true); 
+
+        }
+
 
         return;
       }
