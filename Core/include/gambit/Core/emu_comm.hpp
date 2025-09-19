d0bc2230ec3c1b5e9dbe820e4b68e0fadeb52995
idamarie1303 <idamarie.fauske.johansson@gmail.com>
2025-09-19 13:58:14 +0200

    add a file

---
+++ Core/include/gambit/Core/emu_comm.hpp
@@ -0,0 +1,8 @@
+#ifndef EMU_COMM_HPP
+#define EMU_COMM_HPP
+
+#include <mpi.h> // Make sure MPI is included for MPI_Comm
+
+extern MPI_Comm emuComm; // Declare the global communicator
+
+#endif // EMU_COMM_HPP
