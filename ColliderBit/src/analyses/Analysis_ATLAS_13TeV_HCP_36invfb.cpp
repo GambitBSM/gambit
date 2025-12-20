ed1163e44ba10731a74060f1378506028d55a860
Are Raklev <ahye@uio.no>
2025-12-20 20:28:00 +0100

    Finished HCP skeleton

--- ColliderBit/src/analyses/Analysis_ATLAS_13TeV_HCP_36invfb.cpp
+++ ColliderBit/src/analyses/Analysis_ATLAS_13TeV_HCP_36invfb.cpp
@@ -20,11 +20,10 @@
 #include <fstream>
 
 #include "gambit/ColliderBit/analyses/Analysis.hpp"
-#include "gambit/ColliderBit/analyses/AnalysisMacros.hpp"
+//#include "gambit/ColliderBit/analyses/AnalysisMacros.hpp"
 #include "gambit/ColliderBit/ATLASEfficiencies.hpp"
 #include "gambit/ColliderBit/analyses/Cutflow.hpp"
-//#include "gambit/ColliderBit/mt2_bisect.h"
-//#include "METSignificance/METSignificance.hpp"
+
 
 using namespace std;
 
@@ -36,8 +35,16 @@ namespace Gambit
     class Analysis_ATLAS_13TeV_HCP_36invfb : public Analysis
     {
 
-    public:
+    private:
 
+      // Cut-flows
+      size_t NCUTS;
+      vector<int> cutFlowVector;
+      vector<string> cutFlowVector_str;
+      vector<double> cutFlowVectorATLAS;
+
+    public:
+      
       // Required detector sim
       static constexpr const char* detector = "ATLAS";
 
@@ -45,22 +52,33 @@ namespace Gambit
       {
 
         set_analysis_name("ATLAS_13TeV_HCP_36invfb");
-        set_detector_name(detector);
         set_luminosity(36.1);
-        set_bkgjson("ColliderBit/data/analyses_json_files/ATLAS_13TeV_2LEPJETS_EW_139invfb_full_bkgonly.json");
-
-        // Counters for the number of accepted events for each signal region
-        DEFINE_SIGNAL_REGION("SR-1Cand-mTOF175", "n tight = 1")
-        DEFINE_SIGNAL_REGION("SR-1Cand-mTOF375", "n tight = 1")
-        DEFINE_SIGNAL_REGION("SR-1Cand-mTOF600", "n tight = 1")
-        DEFINE_SIGNAL_REGION("SR-1Cand-mTOF825", "n tight = 1")
         
-        DEFINE_SIGNAL_REGION("SR-2Cand-mTOF150", "n loose = 2")
-        DEFINE_SIGNAL_REGION("SR-2Cand-mTOF350", "n loose = 2")
-        DEFINE_SIGNAL_REGION("SR-2Cand-mTOF575", "n loose = 2")
-        DEFINE_SIGNAL_REGION("SR-2Cand-mTOF800", "n loose = 2")
+        NCUTS=3;
+        for(size_t i=0;i<NCUTS;i++){
+          cutFlowVector.push_back(0);
+          cutFlowVectorATLAS.push_back(0);
+          cutFlowVector_str.push_back("");
+        }
+
       }
 
+    protected:
+      // Signal region map
+      std::map<string, EventCounter> _counters = {
+        // Counters for the number of accepted events for each signal region
+        {"SR-1Cand-mTOF175", EventCounter("SR-1Cand-mTOF175")},
+        {"SR-1Cand-mTOF375", EventCounter("SR-1Cand-mTOF375")},
+        {"SR-1Cand-mTOF600", EventCounter("SR-1Cand-mTOF600")},
+        {"SR-1Cand-mTOF825", EventCounter("SR-1Cand-mTOF825")},
+
+        {"SR-2Cand-mTOF150", EventCounter("SR-2Cand-mTOF150")},
+        {"SR-2Cand-mTOF350", EventCounter("SR-2Cand-mTOF350")},
+        {"SR-2Cand-mTOF575", EventCounter("SR-2Cand-mTOF575")},
+        {"SR-2Cand-mTOF800", EventCounter("SR-2Cand-mTOF800")},
+      };
+
+      
       void run(const HEPUtils::Event* event)
       {
         //
@@ -105,28 +123,21 @@ namespace Gambit
         int nTight = 0;
 
         // Perform all pre-selection cuts
-        BEGIN_PRESELECTION
-        while(true)
-        {
-          // Trigger for missing energy
-          if (met > 110) trigger = true;
+        
+        // Trigger for missing energy
+        if (met > 110) trigger = true;
           
-          // TODO: Trigger for track
+        // TODO: Trigger for track
 
-          // TODO: Count up tight and loose
-          
+        // TODO: Count up tight and loose
 
-          // If it has reached this point, it has passed pre-selection
-          if (trigger) presel = true;
+        // If it has reached this point, it has passed pre-selection
+        if (trigger) presel = true;
 
-          // Applied all cuts
-          break;
-        }
-
+        
         // If event does not pass pre-selection, exit early
         if (presel == false) return;
-        END_PRESELECTION
-
+        
         
         //
         // Signal regions
@@ -137,13 +148,13 @@ namespace Gambit
         // SR with 1 tight candidate
         while (true)
         {
-          if (nTight == 1) { LOG_CUT("Tight1") }
+          if (nTight == 1) cutFlowVector[1]++;
           else break;
           
-          if (mTOF > 175) FILL_SIGNAL_REGION("SR-1Cand-mTOF175");
-          if (mTOF > 375) FILL_SIGNAL_REGION("SR-1Cand-mTOF375");
-          if (mTOF > 600) FILL_SIGNAL_REGION("SR-1Cand-mTOF600");
-          if (mTOF > 825) FILL_SIGNAL_REGION("SR-1Cand-mTOF825");
+          if (mTOF > 175) _counters.at("SR-1Cand-mTOF175").add_event(event);
+          if (mTOF > 375) _counters.at("SR-1Cand-mTOF375").add_event(event);
+          if (mTOF > 600) _counters.at("SR-1Cand-mTOF600").add_event(event);
+          if (mTOF > 825) _counters.at("SR-1Cand-mTOF825").add_event(event);
           
           // Applied all cuts
           break;
@@ -152,13 +163,13 @@ namespace Gambit
         // SR with 2 loose candidates
         while (true)
         {
-          if (nLoose == 2) { LOG_CUT("Loose2") }
+          if (nLoose == 2) cutFlowVector[2]++;
           else break;
           
-          if (mTOF > 150) FILL_SIGNAL_REGION("SR-2Cand-mTOF150");
-          if (mTOF > 350) FILL_SIGNAL_REGION("SR-2Cand-mTOF350");
-          if (mTOF > 575) FILL_SIGNAL_REGION("SR-2Cand-mTOF575");
-          if (mTOF > 800) FILL_SIGNAL_REGION("SR-2Cand-mTOF800");
+          if (mTOF > 150) _counters.at("SR-2Cand-mTOF150").add_event(event);
+          if (mTOF > 350) _counters.at("SR-2Cand-mTOF350").add_event(event);
+          if (mTOF > 575) _counters.at("SR-2Cand-mTOF575").add_event(event);
+          if (mTOF > 800) _counters.at("SR-2Cand-mTOF800").add_event(event);
           
           // Applied all cuts
           break;
@@ -167,29 +178,60 @@ namespace Gambit
       } // End run function
 
 
+      /// Combine the variables of another copy of this analysis (typically on another thread) into this one.
+      void combine(const Analysis* other)
+      {
+        const Analysis_ATLAS_13TeV_HCP_36invfb* specificOther
+          = dynamic_cast<const Analysis_ATLAS_13TeV_HCP_36invfb*>(other);
+
+        for (auto& pair : _counters) { pair.second += specificOther->_counters.at(pair.first); }
+
+        if (NCUTS != specificOther->NCUTS) NCUTS = specificOther->NCUTS;
+        for (size_t j=0; j<NCUTS; j++) {
+          cutFlowVector[j] += specificOther->cutFlowVector[j];
+          cutFlowVector_str[j] = specificOther->cutFlowVector_str[j];
+        }
+      }
+
       // This function can be overridden by the derived SR-specific classes
       virtual void collect_results()
       {
-        COMMIT_SIGNAL_REGION("SR-1Cand-mTOF175", 227, 240,     20);
-        COMMIT_SIGNAL_REGION("SR-1Cand-mTOF375",  16,  17,      2);
-        COMMIT_SIGNAL_REGION("SR-1Cand-mTOF600",   1,   2.2,    0.2);
-        COMMIT_SIGNAL_REGION("SR-1Cand-mTOF825",   0,   0.48,   0.07);
-        COMMIT_SIGNAL_REGION("SR-2Cand-mTOF150",   0.,  1.5,    0.3);
-        COMMIT_SIGNAL_REGION("SR-2Cand-mTOF350",   0.,  0.06,   0.01);
-        COMMIT_SIGNAL_REGION("SR-2Cand-mTOF575",   0.,  0.007,  0.002);
-        COMMIT_SIGNAL_REGION("SR-2Cand-mTOF800",   0.,  0.0017, 0.0009);
+        
+        cout << "CUTFLOW" << endl;
+        double L = 36.1;
+        double xsec = 3.460;
+        for (size_t i=0; i<NCUTS; i++)
+        {
+          double ATLAS_abs = cutFlowVectorATLAS[i];
+          double eff = (double)cutFlowVector[i] / (double)cutFlowVector[0];
+          double GAMBIT_scaled = eff * xsec * L;
+          double ratio = GAMBIT_scaled/ATLAS_abs;
+          cout << "CUTFLOW " << i << ":   " << setprecision(4) << ATLAS_abs << "\t" << GAMBIT_scaled << "\t" << "\t" << ratio << "\t\t" << cutFlowVector_str[i] << endl;
+        }
+        cout << "CUTFLOW" << endl;
 
-        COMMIT_CUTFLOWS
+        add_result(SignalRegionData(_counters.at("SR-1Cand-mTOF175"), 227, {240,     20}));
+        add_result(SignalRegionData(_counters.at("SR-1Cand-mTOF175"),  16, { 17,      2}));
+        add_result(SignalRegionData(_counters.at("SR-1Cand-mTOF175"),   1, {  2.2,    0.2}));
+        add_result(SignalRegionData(_counters.at("SR-1Cand-mTOF175"),   0, {  0.48,   0.07}));
+        add_result(SignalRegionData(_counters.at("SR-2Cand-mTOF150"),   0, {  1.5,    0.3}));
+        add_result(SignalRegionData(_counters.at("SR-2Cand-mTOF350"),   0, {  0.06,   0.01}));
+        add_result(SignalRegionData(_counters.at("SR-2Cand-mTOF575"),   0, {  0.007,  0.002}));
+        add_result(SignalRegionData(_counters.at("SR-2Cand-mTOF800"),   0, {  0.0017, 0.0009}));
+
       }
 
-    protected:
-//      void analysis_specific_reset()
-//      {
-//        for (auto& pair : _counters)
-//        {
-//          pair.second.reset();
-//        }
-//      }
+      void analysis_specific_reset()
+      {
+        // Clear signal regions
+        for (auto& pair : _counters)
+        {
+          pair.second.reset();
+        }
+        
+        // Clear cut flow vector
+        std::fill(cutFlowVector.begin(), cutFlowVector.end(), 0);
+      }
 
     };
 
