c2fd303783315e074befad50d57e78017d15a248
Pengxuan Zhu <zhupx99@icloud.com>
2026-08-21 02:02:37 +0930

    Remove contrib/fjcore, tested by P.Zhu

--- cmake/contrib.cmake
+++ cmake/contrib.cmake
@@ -595,24 +595,10 @@ else()
   set(WITH_FASTJET_CONTRIB FALSE)
 endif()
 
-#contrib/fjcore-3.2.0
-# ColliderBit builds full FastJet, so FJNS is fastjet and fjcore is unused.
-# Keep the object library only when FastJet is not in the build.
+# FastJet namespace used by HEPUtils and ColliderBit. There is no fjcore
+# fallback: jet clustering requires the full FastJet contrib build above.
 if(WITH_FASTJET_CONTRIB)
   add_definitions(-DFJNS=fastjet)
-else()
-  set(fjcore_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/contrib/fjcore-3.2.0")
-  include_directories("${fjcore_INCLUDE_DIR}")
-  add_definitions(-DFJCORE)
-  add_definitions(-DFJNS=gambit::fjcore)
-  add_gambit_library(fjcore OPTION OBJECT
-                            SOURCES ${PROJECT_SOURCE_DIR}/contrib/fjcore-3.2.0/fjcore.cc
-                            HEADERS ${PROJECT_SOURCE_DIR}/contrib/fjcore-3.2.0/fjcore.hh)
-  set(GAMBIT_BASIC_COMMON_OBJECTS "${GAMBIT_BASIC_COMMON_OBJECTS}" $<TARGET_OBJECTS:fjcore>)
-  add_dependencies(contrib fjcore)
-endif()
-
-if(WITH_FASTJET_CONTRIB)
   set(fjcontrib_nsubjettiness_dir "${fjcontrib_path}/Nsubjettiness")
   set(fjcontrib_nsubjettiness_sources
       ${fjcontrib_nsubjettiness_dir}/AxesDefinition.cc
